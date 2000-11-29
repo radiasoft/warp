52e7fc1efbfcb2c8e3783580332332d19875270b
dave <dave>
2000-11-29 03:07:59 +0000

    Original version

---
+++ scripts/gistdummy.py
@@ -0,0 +1,60 @@
+# Copyright (c) 1996, 1997, The Regents of the University of California.
+# All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
+gistdummy_version = "$Id:"
+from Numeric import *
+import sys, os	# To be sure expand_path has posixpath and we have sys.path
+#from gistC import *
+from Numeric import *
+from help import help
+from shapetest import *
+from arrayfns import *
+from types import *
+
+def hcp_file(*x,**x):
+  pass
+def hcpon(*x,**x):
+  pass
+def hcp(*x,**x):
+  pass
+def window(*x,**x):
+  pass
+def plg(*x,**x):
+  pass
+def fma():
+  pass
+def plt(titlel="",x=0,y=0,justify="CC",orient=1):
+  pass
+def plsys(i=0):
+  pass
+def winkill(*N):
+  pass
+def pltitle(title):
+  pass
+def limits(*x,**x):
+  pass
+def ylimits(ymin='u',ymax='u'):
+  pass
+def moush(*arg):
+  pass
+def eps(name):
+  import os
+  pass
+def xytitles(xtitle = "", ytitle = "", delta = (0.,0.)):
+  pass
+def _spanz(lb,ub,n):
+  pass
+def plmk(y,x=None,marker=None,width=None,color=None,msize=None):
+  pass
+def spann (zmin, zmax, n = 8, fudge = 0, force = 0) :
+  pass
+def plfc (z, y, x, ireg, contours = 20, colors = None, region = 0,
+          triangle = None, scale = "lin") :
+  pass
+def plc(*x,**x):
+  pass
+def redraw():
+  pass
+def current_window():
+  pass
+def pldefault(*x,**x):
+  pass
