00f62fccc46c2e663dbbdef4f882005bebfde2df
dave <dave>
2001-03-23 00:02:28 +0000

    Now saves bot sum() and sum(abs()).

--- scripts/setupvalidation.py
+++ scripts/setupvalidation.py
@@ -1,5 +1,5 @@
 from warp import *
-setupvalidation_version = "$Id: setupvalidation.py,v 1.2 2001/03/22 23:24:04 dave Exp $"
+setupvalidation_version = "$Id: setupvalidation.py,v 1.3 2001/03/23 00:02:28 dave Exp $"
 
 def setupvalidationdoc():
   print """
@@ -14,13 +14,6 @@ instructions.
 def getvalidationdata():
   """Returns the current values of the quantities to be compared."""
   if lparallel:
-    sumxp = globalsum(sum(abs(getx(gather=0))))
-    sumyp = globalsum(sum(abs(gety(gather=0))))
-    sumzp = globalsum(sum(abs(getz(gather=0))))
-    sumvx = globalsum(sum(abs(getvx(gather=0))))
-    sumvy = globalsum(sum(abs(getvy(gather=0))))
-    sumvz = globalsum(sum(abs(getvz(gather=0)-top.vbeam)))
-    sumgi = globalsum(sum(abs(1.-getgaminv(gather=0))))
     # --- Sum only the non overlapping part of the 3-d arrays. Otherwise
     # --- the answers will be different with differing numbers of processors.
     iz1 = top.izfsslave[me] - top.izslave[me]
@@ -28,19 +21,39 @@ def getvalidationdata():
       iz2 = top.izfsslave[me+1] - top.izslave[me]
     else:
       iz2 = iz1 + top.nzfsslave[me] + 1
-    sumrho = globalsum(sum(sum(sum(w3d.rho[:,:,iz1:iz2]))))
-    sumphi = globalsum(sum(sum(sum(w3d.phi[:,:,iz1+1:iz2+1]))))
+    return (globalsum(sum(getx(gather=0))),
+            globalsum(sum(gety(gather=0))),
+            globalsum(sum(getz(gather=0))),
+            globalsum(sum(getvx(gather=0))),
+            globalsum(sum(getvy(gather=0))),
+            globalsum(sum(getvz(gather=0)-top.vbeam)),
+            globalsum(sum(1.-getgaminv(gather=0))),
+            globalsum(sum(abs(getx(gather=0)))),
+            globalsum(sum(abs(gety(gather=0)))),
+            globalsum(sum(abs(getz(gather=0)))),
+            globalsum(sum(abs(getvx(gather=0)))),
+            globalsum(sum(abs(getvy(gather=0)))),
+            globalsum(sum(abs(getvz(gather=0)-top.vbeam))),
+            globalsum(sum(abs(1.-getgaminv(gather=0)))),
+            globalsum(sum(sum(sum(w3d.rho[:,:,iz1:iz2])))),
+            globalsum(sum(sum(sum(w3d.phi[:,:,iz1+1:iz2+1])))))
   else:
-    sumxp = sum(abs(getx()))
-    sumyp = sum(abs(gety()))
-    sumzp = sum(abs(getz()))
-    sumvx = sum(abs(getvx()))
-    sumvy = sum(abs(getvy()))
-    sumvz = sum(abs(getvz()-top.vbeam))
-    sumgi = sum(abs(1.-getgaminv()))
-    sumrho = sum(sum(sum(w3d.rho)))
-    sumphi = sum(sum(sum(w3d.phi)))
-  return (sumxp,sumyp,sumzp,sumvx,sumvy,sumvz,sumgi,sumrho,sumphi)
+    return (sum(getx()),
+            sum(gety()),
+            sum(getz()),
+            sum(getvx()),
+            sum(getvy()),
+            sum(getvz()-top.vbeam),
+            sum(1.-getgaminv()),
+            sum(abs(getx())),
+            sum(abs(gety())),
+            sum(abs(getz())),
+            sum(abs(getvx())),
+            sum(abs(getvy())),
+            sum(abs(getvz()-top.vbeam)),
+            sum(abs(1.-getgaminv())),
+            sum(sum(sum(w3d.rho))),
+            sum(sum(sum(w3d.phi))))
 
 ##########################################################################
 def setupvalidator():
@@ -52,39 +65,42 @@ is to be done.
 
 # ---
 from setupvalidation import comparetooriginal
-comparetooriginal(
+comparetooriginal([
 %22.15e,%22.15e,%22.15e,
 %22.15e,%22.15e,%22.15e,
-%22.15e,%22.15e,%22.15e
+%22.15e,
+%22.15e,%22.15e,%22.15e,
+%22.15e,%22.15e,%22.15e,
+%22.15e,
+%22.15e,%22.15e]
 )
 """
   originaldata = getvalidationdata()
   print output%originaldata
 
 ##########################################################################
-def comparetooriginal(sumxp,sumyp,sumzp,sumvx,sumvy,sumvz,sumgi,sumrho,sumphi):
+def comparetooriginal(odata):
   """Prints out the comparison between the original data and the current
   data."""
-  fmt0 = "          %-22s %-22s %-22s"
-  fmt1 = "          %22.15e %22.15e %22.15e"
-  fmt2 = "original  %22.15e %22.15e %22.15e"
-  otext = ["sum(x)","sum(y)","sum(z)",
-           "sum(vx)","sum(vy)","sum(vz-vbeam)",
-           "sum(1-gaminv)","sum(rho)","sum(phi)"]
+  fmt1 = "          %22.15e %22.15e"
+  fmt2 = "original  %22.15e %22.15e"
+  otext = ["x","y","z","vx","vy","vz-vbeam","1-gaminv",
+           "x","y","z","vx","vy","vz-vbeam","1-gaminv",
+           "rho","phi"]
   vdata = array(getvalidationdata())
-  odata = array([sumxp,sumyp,sumzp,sumvx,sumvy,sumvz,sumgi,sumrho,sumphi])
-  print fmt0%(otext[0],otext[1],otext[2])
-  print fmt1%(vdata[0],vdata[1],vdata[2])
-  print fmt2%(odata[0],odata[1],odata[2])
+  odata = array(odata)
+  print "         %10s %-22s %-22s"%(" ","sum()","sum(abs())")
+  for i in range(7):
+    print "         %10s %22.15e %22.15e"%(otext[i],vdata[i],vdata[i+7])
+    print "original %10s %22.15e %22.15e"%(otext[i],odata[i],odata[i+7])
+    print
+
+  print "         %10s %-22s"%(" ","sum()")
+  print "         %10s %22.15e"%(otext[14],vdata[14])
+  print "original %10s %22.15e"%(otext[14],odata[14])
   print
-  print fmt0%(otext[3],otext[4],otext[5])
-  print fmt1%(vdata[3],vdata[4],vdata[5])
-  print fmt2%(odata[3],odata[4],odata[5])
-  print
-  print fmt0%(otext[6],otext[7],otext[8])
-  print fmt1%(vdata[6],vdata[7],vdata[8])
-  print fmt2%(odata[6],odata[7],odata[8])
-  print
+  print "         %10s %22.15e"%(otext[15],vdata[15])
+  print "original %10s %22.15e"%(otext[15],odata[15])
 
 # diffs = abs(odata - vdata)
 # if max(diffs) > 0.:
