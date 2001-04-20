fc052c5c7de2c23064fe52e3dc876ebd201614aa
dave <dave>
2001-04-20 00:19:46 +0000

    Added ISZ definition for the size of integers and logicals. On most machines, the size is 4 bytes, but on 64-bit machines, the size is 8 bytes. The size should always correspond to the size of a long. Also changed comment character from 'c' to '!' since this file is used by both the fixed and free format files.

--- pywarp90/top.h
+++ pywarp90/top.h
@@ -2,23 +2,23 @@
 #define MACHEPS 1.0e-14
 #define LARGEPOS 1.0e+36
 #define SMALLPOS 1.0e-36
-c Length of diagnostic control arrays
+! Length of diagnostic control arrays
 #define NCONTROL 50
-c Default length of arrays describing lattice
+! Default length of arrays describing lattice
 #define NELEMENT 100
-c Number of particle per group
+! Number of particle per group
 #define NPARPGRP 256
-c Max number of ptcl "subsets" for scatter plots
+! Max number of ptcl "subsets" for scatter plots
 #define NSUBSETS 3
-c Max number of diagnostic windows
+! Max number of diagnostic windows
 #define NWINDOWS 9
-c Number of z moments
+! Number of z moments
 #define NUMZMMNT 28
-c Max number of lab windows
+! Max number of lab windows
 #define MAXNUMLW 50
-c Used only for data statements
+! Used only for data statements
 #define TNWINM  2*NWINDOWS
-c Used only for data statements
+! Used only for data statements
 #define NWPNSP1 NWINDOWS + NSUBSETS + 1
 #define NEVER   0
 #define SELDOM  1
@@ -27,3 +27,10 @@ c Used only for data statements
 #define OK 0
 #define STDOUT 6
 #define dvnz(X) sign(abs(X)+SMALLPOS,X)
+
+! Define size of integers. Must be the same as the size of a long int in C.
+#ifdef ALPHA
+#define ISZ 8
+#else
+#define ISZ 4
+#endif
