8afbf30a1962e8925fe4050dcaf9dc2875e8cd3d
dave <dave>
2001-06-19 01:05:09 +0000

    Minor change to calls to condbndy in psor3d. Arrays are passed in via references to their first elements. This fixes a problem of unknown cause that causes segmentation faults under certain circumstance (during a restart). This change will be undone at some point when the problem is tracked down.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.6 $, $Date: 2001/06/01 22:37:15 $
+#@(#) File F3D.V, version $Revision: 3.7 $, $Date: 2001/06/19 01:05:09 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -9,7 +9,7 @@ f3d
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.6 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.7 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
