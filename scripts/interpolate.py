df8c19a44e98f497d9df57019868e0146fbb5393
dave <dave>
2001-07-09 20:39:40 +0000

    Added function interpolate3d which gets phi at the specified location via linear interpolation. This function should really be a part of getphi.

--- scripts/interpolate.py
+++ scripts/interpolate.py
@@ -1,5 +1,5 @@
 from warp import *
-interepolate_version = "$Id: interpolate.py,v 1.1 2000/10/16 18:34:19 dave Exp $"
+interepolate_version = "$Id: interpolate.py,v 1.2 2001/07/09 20:39:40 dave Exp $"
 
 def interpolate(iw=0,js=1,win=None,z=None,iz=None,wz=1.,zl=None,zu=None,
                 serial=1):
@@ -23,4 +23,22 @@ def interpolate(iw=0,js=1,win=None,z=None,iz=None,wz=1.,zl=None,zu=None,
   yy = yy + vy*delt
   zz = zz + vz*delt
   return (xx,yy,zz,vx,vy,vz,gg)
+
+
+# --- Get a value at a specified location using linear interpolation
+def interpolate3d(x,y,z):
+  ix = int((x - w3d.xmmin)/w3d.dx)
+  iy = int((y - w3d.ymmin)/w3d.dy)
+  iz = int((z - w3d.zmmin)/w3d.dz)
+  wx = (x - w3d.xmmin)/w3d.dx - ix
+  wy = (y - w3d.ymmin)/w3d.dy - iy
+  wz = (z - w3d.zmmin)/w3d.dz - iz
+  return w3d.phi[ix  ,iy  ,iz+1  ]*(1.-wx)*(1.-wy)*(1.-wz) + \
+         w3d.phi[ix+1,iy  ,iz+1  ]*    wx *(1.-wy)*(1.-wz) + \
+         w3d.phi[ix  ,iy+1,iz+1  ]*(1.-wx)*    wy *(1.-wz) + \
+         w3d.phi[ix+1,iy+1,iz+1  ]*    wx *    wy *(1.-wz) + \
+         w3d.phi[ix  ,iy  ,iz+1+1]*(1.-wx)*(1.-wy)*    wz  + \
+         w3d.phi[ix+1,iy  ,iz+1+1]*    wx *(1.-wy)*    wz  + \
+         w3d.phi[ix  ,iy+1,iz+1+1]*(1.-wx)*    wy *    wz  + \
+         w3d.phi[ix+1,iy+1,iz+1+1]*    wx *    wy *    wz
 
