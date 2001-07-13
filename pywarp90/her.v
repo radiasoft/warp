eaf2eea8d8d875e8ef025cfa76e61e9eb7462ccd
mdehoon <mdehoon>
2001-07-13 00:37:34 +0000

    Centroid motion and image charge effects included.

--- pywarp90/her.v
+++ pywarp90/her.v
@@ -1,5 +1,5 @@
 her
-#@(#) File HER.V, version $Revision: 3.5 $, $Date: 2001/07/12 00:03:59 $
+#@(#) File HER.V, version $Revision: 3.6 $, $Date: 2001/07/13 00:37:34 $
 # Copyright (c) 1999, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for HERMES.
@@ -9,7 +9,7 @@ her
 
 *********** HERversion:
 # Version control for her package
-versher       character*19 /"$Revision: 3.5 $"/  # Current code version, set by SCCS
+versher       character*19 /"$Revision: 3.6 $"/  # Current code version, set by SCCS
 
 *********** HERvars dump:
 # Variables needed by the package HER
@@ -66,7 +66,7 @@ iprofile   integer /0/ # line-charge profile flag
 lfail      logical /.false./ # Set to true if a time step fails
 
 *********** HERfield:
-# Fields and forces at the current time step
+# Fields and forces at the current time step, for the different slices
 nizfield             integer # Size of arrays
 dedx(nizfield)       _real # From quadrupole
 dbdx(nizfield)       _real # From quadrupole
@@ -74,8 +74,22 @@ er(nizfield)         _real # Radial electric field (n = 0, v = 0)
 ez(nizfield)         _real # Longitudinal electric field (n = 0, v = 0)
 br(nizfield)         _real # Radial magnetic field (n = 0, v = 0)
 bz(nizfield)         _real # Longitudinal magnetic field (n = 0, v = 0)
+# Note: er,bz,br are ignored for now
 ezbeam(nizfield)     _real [V/m] # Self axial electric field
-rpipe(nizfield)      _real # Pipe radius at the z-location of the different slices
+fx(nizfield)         _real # From image
+fy(nizfield)         _real # From image
+gxx(nizfield)        _real # From image
+gxy(nizfield)        _real # From image
+gyx(nizfield)        _real # From image
+gyy(nizfield)        _real # From image
+ipipetype            integer /0/ # Defines the pipe shape used for image force calculations
+                                 # 0 round pipe (default)
+                                 # 1 electric quadrupole rods (hyperbolic sufaces)
+                                 # 2 dipole plates (parallel plates)
+rpipe(nizfield)      _real [m] # Pipe radius at the z-location of the different slices
+pipeox(nizfield)   _real /0.0/ [m] # Horizontal offset of the pipe (needed for images)
+pipeoy(nizfield)   _real /0.0/ [m] # Vertical offset of the pipe (needed for images)
+pipeph(nizfield)   _real /0.0/ [rad] # Rotation angle of the pipe (needed for images)
 
 *********** HERvartmp:
 # These are used as temporary space in the integration of the envelope
@@ -130,12 +144,12 @@ extebher(t:real,dt:real,y:real,niz:integer) subroutine
   # Gather applied fields
 getderivs(t:real,dt:real,y:real,dy:real,niz:integer,
        aion:real,zion:real,fviscous:real,
-       icharge:integer,lezbeam:logical,lperveance:logical,lemittance:logical,
+       icharge:integer,iimage:integer,lezbeam:logical,lperveance:logical,lemittance:logical,
        lallez:logical,lezcenter:logical,lviscous:logical,lcurgrid:logical,lfail:logical) subroutine
   # Calculates new envelope quantities using applied and self fields
 stephr(y:real,dy:real,niz:integer,t:real,dt:real,
         aion:real,zion:real,fviscous:real,
-        icharge:integer,lezbeam:logical,lperveance:logical,
+        icharge:integer,iimage:integer,lezbeam:logical,lperveance:logical,
         lemittance:logical,lallez:logical,lezcenter:logical,
         lviscous:logical,lcurgrid:logical,lfail:logical) subroutine
   # One complete isochronous leapfrog integration step
@@ -154,6 +168,8 @@ getcurrent(y:real,niz:integer,rpipe:real,icharge:integer,lcurgrid:logical,
 savehermesvars(t:real,y:real,niz:integer,it:integer,lsavehist:logical)
     subroutine
   # Checks if history needs to be saved and saves it
+getimage(y:real,niz:integer,iimage:integer,lfail:logical) subroutine
+  # Calculates the image effects on the transverse envelope
 copyhermesvars() subroutine
    # Copies the her.var variables into the separated arrays
 resizehermeshist() subroutine
