7f9f876b78e0b51a0b18cc7bc39772e62168a26c
dave <dave>
2001-07-18 23:55:30 +0000

    Now zlmin and zlmax are explicitly set to zero before the call to setlatt in case they were set in another package.

--- pywarp90/wxy.v
+++ pywarp90/wxy.v
@@ -1,5 +1,5 @@
 wxy
-#@(#) File WXY.V, version $Revision: 3.9 $, $Date: 2001/07/10 20:35:33 $
+#@(#) File WXY.V, version $Revision: 3.10 $, $Date: 2001/07/18 23:55:31 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package WXY of code WARP
@@ -9,7 +9,7 @@ wxy
 
 *********** WXYversion:
 # Quantities associated with version control 
-verswxy character*19 /"$Revision: 3.9 $"/ # Current code version, set by CVS
+verswxy character*19 /"$Revision: 3.10 $"/ # Current code version, set by CVS
 
 *********** Particlesxy dump parallel:
 npmaxxy    integer # Number of particles - same as npmax from TOP
