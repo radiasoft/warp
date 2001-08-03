0342643552052dad5d33b6c69239822757eb6ff2
jlvay <jlvay>
2001-08-03 00:46:58 +0000

    matching version numbers

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.7 $, $Date: 2001/06/19 01:05:09 $
+#@(#) File F3D.V, version $Revision: 3.8 $, $Date: 2001/08/03 00:47:30 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -9,7 +9,7 @@ f3d
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.7 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.8 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
