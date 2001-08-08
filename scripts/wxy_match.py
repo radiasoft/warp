a567148d08fb2f336b8c8615703580a85c51fe01
dave <dave>
2001-08-08 18:21:26 +0000

    Added MatchXY class which is derived from the matchpic set of tools developed by Micheal deHoon. Provides a tool to match a beam to end conditions by varying quadrupole strengths.

--- scripts/wxy_match.py
+++ scripts/wxy_match.py
@@ -1,7 +1,21 @@
 from warp import *
 import LinearAlgebra
 import singleparticle
-wxy_match_version = "$Id: wxy_match.py,v 1.3 2001/06/14 01:14:29 dave Exp $"
+wxy_match_version = "$Id: wxy_match.py,v 1.4 2001/08/08 18:21:26 dave Exp $"
+
+def wxy_matchdoc():
+  print """
+Contains tools for matching beams using the WARPxy code.
+minit: re-generates the slice beam
+match: varies the beam parameters to finds a matched beam (first method)
+match1: varies the beam parameters to finds a matched beam (second method)
+matchx: varies single particle starting conditions to match final conditions
+MatchXY: class to match a beam from a given set of beam parameters to a final
+         set of beam parameters by changing the focusing strength on four
+         quadrupoles. So far, only linear quadrupole elements can be used.
+  """
+
+
 ##############################################################################
 # Iterate toward a matched beam using the slice code (instead of the env     #
 # package).  It is currently setup to match a beam starting from the center  #
@@ -176,4 +190,227 @@ def matchx(xf=0.,xpf=0.,yf=0.,ypf=0.,zs=None,ze=None,s=None,
 
 
 
+#############################################################################
+#############################################################################
+#############################################################################
+#############################################################################
+#############################################################################
+
+class MatchXY:
+  """
+Matches a beam to final conditions.
+  - afnew: final value of a
+  - bfnew: final value of b
+  - apfnew: final value of a'
+  - bpfnew: final value of b'
+  - runsimulation: is a Python function that runs the WarpXY simulation from
+                   the starting location to the final location. Before calling
+                   iterate, WarpXY should be initialized such that the
+                   simulation can run 
+  - iquads=[0,1,2,3]: indeces of the quad elements to be varied
+  - tolx=1.0e-4: Minimum relative change in quad strengths for the iteration
+                 to continue
+  - alf=1.0e-4: Ensures sufficient decrease in function value
+  - eps=1.0e-8: Approximate square root of the machine precision.
+  - stpmax=400.0:
+Available functions:
+  - iterate(n): perform the iterations
+Available data:
+  - herror: list of the errors after each iteration
+    """
+  def __init__(s,afnew,bfnew,apfnew,bpfnew,runsimulation,iquads=[0,1,2,3],
+                 tolx=1.0e-4,alf=1.0e-4,eps=1.0e-8,stpmax=400.0):
+    # --- Save the beam parameters to be matched to
+    s.af = afnew
+    s.bf = bfnew
+    s.apf = apfnew
+    s.bpf = bpfnew
+    # --- Find the quads to be varied
+    s.iquads = iquads
+    s.savedquadde = zeros (4,'d')
+    s.savedquaddb = zeros (4,'d')
+    try:
+      s.savedquadde = take(top.quadde,s.iquads)
+      s.savedquaddb = take(top.quaddb,s.iquads)
+    except IndexError:
+      raise "Error: iquads index out of bounds"
+    # --- Save initial aperture and grid size
+    s.initialaperture = top.prwall
+    s.initialxmmax = w3d.xmmax
+    # --- Save stepper function
+    s.propagate = runsimulation
+    # --- Set some constants
+    s.tolx = tolx
+    s.alf = alf
+    s.eps = eps
+    s.stpmax = stpmax
+    # --- Initial step has not been done yet
+    s.firststepcomplete = 0
+    s.niterations = 0
+    s.herror = []
+
+  def iterate(s,n=1):
+    """Perform the iterations. Optional argument is the number of iterations
+    to do"""
+    # --- Do initial iteration if it hasn't been done yet.
+    if not s.firststepcomplete:
+      error = s.firststep()
+      n = n - 1
+      s.niterations = s.niterations + 1
+      s.firststepcomplete = 1
+      s.herror.append(error)
+      print 'Initial error is %f'%(error)
+    # --- Do more iterations
+    for i in xrange(n):
+      error = s.nextstep()
+      s.niterations = s.niterations + 1
+      s.herror.append(error)
+      print 'After %d iterations, error = %f'%(s.niterations,errro)
+
+  def reset(s,initialaperture,initialxmmax):
+    # --- Re-initialize the slice package
+    top.lprntpara = false
+    top.verbosity = 0
+    top.zbeam = 0.
+    top.zgrid = 0.
+    top.zgridprv = 0.
+    top.it = 0.
+    top.time = 0.
+    top.jhist = -1
+    # --- Set grid size in the starting channel
+    top.prwallz = initialaperture
+    w3d.xmmax =  initialxmmax
+    w3d.ymmax =  w3d.xmmax
+    w3d.xmmin = -w3d.xmmax
+    w3d.ymmin = -w3d.ymmax
+    if (w3d.l2symtry):
+      w3d.ymmin = 0
+    if (w3d.l4symtry):
+      w3d.xmmin = 0
+      w3d.ymmin = 0
+    w3d.dx = (w3d.xmmax - w3d.xmmin)/w3d.nx
+    w3d.dy = (w3d.ymmax - w3d.ymmin)/w3d.ny
+    # --- Generate the XY code (allocate storage, load ptcls, t=0 plots, etc.).
+    w3d.nz = 2
+    w3d.zmmin = 0.
+    w3d.zmmax = 0. # Needed for parallel running
+    package ("wxy"); generate()
+    fieldsolxy(0)
+
+  def firststep(s):
+    # --- Calculate the initial error
+    print '### Calculating the initial error...'
+    reset(s.initialaperture,s.initialxmmax)
+    s.propagate()
+    if top.nplive < top.npmax:
+      print "Particles lost, remaining number is", top.nplive
+    s.fvec = array([2*top.xrms[0]-s.af,
+                    2*top.yrms[0]-s.bf,
+                    2*top.xxpbar[0]/top.xrms[0]-s.apf,
+                    2*top.yypbar[0]/top.yrms[0]-s.bpf])
+    s.f = 0.5 * sum (s.fvec*s.fvec)
+    error = sqrt(2.*s.f)
+    s.scaling = array ([1.0, 1.0, 1.0, 1.0])
+    return error
+
+  def nextstep(s):
+    """
+    Performs one iteration step, and returns the error.
+    No arguments are needed.
+    """
+    # --- Vary each quad
+    print '### Calculating the jacobian matrix'
+    fjac = zeros ((4,4),'d')
+    g = zeros ((4),'d')
+    p = zeros ((4),'d')
+    xold = zeros ((4),'d')
+    for i in range (4):
+      temp = s.scaling[i]
+      h = s.eps * abs(temp)
+      if (h == 0.0): h = s.eps
+      # --- Trick to reduce finite precision error.
+      s.scaling[i] = temp + h
+      h = s.scaling[i] - temp
+      for j in range (4):
+        iq = s.iquads[j]
+        top.quadde[iq] = s.savedquadde[j] * s.scaling[j]
+        top.quaddb[iq] = s.savedquaddb[j] * s.scaling[j]
+      reset(s.initialaperture,s.initialxmmax)
+      s.propagate()
+      if top.nplive < top.npmax:
+        print "Particles lost, remaining number is", top.nplive
+      fnew = array([2*top.xrms[0]-s.af,
+                    2*top.yrms[0]-s.bf,
+                    2*top.xxpbar[0]/top.xrms[0]-s.apf,
+                    2*top.yypbar[0]/top.yrms[0]-s.bpf])
+      s.scaling[i] = temp
+      # --- Forward difference formula.
+      fjac[:,i] = (fnew[:]-s.fvec[:]) / h
+      g[i] = sum (fjac[:,i]*s.fvec[:])
+    xold[:] = s.scaling[:]
+    p[:] = -s.fvec[:]
+    p = LinearAlgebra.solve_linear_equations (fjac, p)
+    # --- Starting linesearch
+    fold = s.f
+    total = sqrt(sum(p[:]*p[:]))
+    # --- Scale if attempted step is too big.
+    if (total>s.stpmax): p[:] = p[:] * s.stpmax / total
+    slope = sum (g[:]*p[:])
+    if (slope >= 0.0): raise 'Roundoff problem in lnsrch'
+    junk = zeros ((4),'d')
+    for i in range(4): junk[i] = abs (p[i]) / max (abs(xold[i]),1.0)
+    # --- Compute lambda_min
+    test = max (junk[:])
+    alamin = s.tolx / test
+    # --- Always try full Newton step first.
+    alam = 1.0
+    f2 = 0
+    alam2 = 0
+    completed = 0
+    # --- Start of iteration loop.
+    while (not completed):
+      tmplam = 0
+      for i in range(4):
+        iq = s.iquads[i]
+        s.scaling[i] = xold[i] + alam * p[i]
+        top.quadde[iq] = s.savedquadde[i] * s.scaling[i]
+        top.quaddb[iq] = s.savedquaddb[i] * s.scaling[i]
+      print '### Running with new quad settings'
+      reset(s.initialaperture,s.initialxmmax)
+      s.propagate()
+      if top.nplive < top.npmax:
+        print "Particles lost, remaining number is", top.nplive
+      s.fvec = array([2*top.xrms[0]-s.af,
+                      2*top.yrms[0]-s.bf,
+                      2*top.xxpbar[0]/top.xrms[0]-s.apf,
+                      2*top.yypbar[0]/top.yrms[0]-s.bpf])
+      total = sum (s.fvec[:]*s.fvec[:])
+      s.f = 0.5 * total
+      # --- Convergence on delta x.
+      if (alam < alamin): completed = 1
+      # --- Sufficient function decrease
+      elif (s.f <= fold + s.alf * alam * slope): completed = 1
+      # --- Backtrack first time
+      elif (alam == 1.0): tmplam = - slope / (2.0 * (s.f-fold-slope))
+      # --- Subsequent backtracks.
+      else:
+        rhs1 = s.f - fold - alam * slope
+        rhs2 = f2 - fold - alam2 * slope
+        a = (rhs1/(alam*alam)-rhs2/(alam2*alam2))/(alam-alam2)
+        b = (-alam2*rhs1/(alam*alam)+alam*rhs2/(alam2*alam2))/(alam-alam2)
+        if (a == 0.0): tmplam = - slope / (2.0 * b)
+        else:
+          disc = b*b-3.0*a*slope
+          if (disc < 0.0): tmplam = 0.5 * alam
+          elif (b <= 0.0): tmplam = (-b + sqrt(disc))/(3.0*a)
+          else: tmplam = - slope / (b + sqrt(disc))
+        # --- lambda <= 0.5 lambda1
+        if (tmplam > 0.5 * alam): tmplam = 0.5 * alam
+      alam2 = alam
+      f2 = s.f
+      # --- lambda >= 0.1 lambda1
+      alam = max (tmplam,0.1*alam)
+    # --- End of linesearch
+    error = sqrt(2.*s.f)
+    return error
 
