cf48ac23331870940a0143528dc7ab76a8b04e2f
jlvay <jlvay>
2001-08-15 16:45:25 +0000

    rearranged loops in subroutine inject3d for better efficiency when using multiple sources with multiple species

--- pywarp90/w3d.v
+++ pywarp90/w3d.v
@@ -1,5 +1,5 @@
 w3d
-#@(#) File W3D.V, version $Revision: 3.19 $, $Date: 2001/08/14 20:32:09 $
+#@(#) File W3D.V, version $Revision: 3.20 $, $Date: 2001/08/15 16:45:25 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package W3D of code WARP
@@ -9,7 +9,7 @@ w3d
 
 *********** W3Dversion:
 # Quantities associated with version control 
-versw3d character*19 /"$Revision: 3.19 $"/ # Current code version, set by CVS
+versw3d character*19 /"$Revision: 3.20 $"/ # Current code version, set by CVS
 
 *********** InPltCtl3d dump:
 # Controls for when the various plots are made
