4e766d3f42f85acd41ef8c72a1382ba7899e39ef
dave <dave>
2001-09-19 22:32:28 +0000

    Bug fix: corrected argument list in calls to applyacclxy.

--- pywarp90/wxy.v
+++ pywarp90/wxy.v
@@ -1,5 +1,5 @@
 wxy
-#@(#) File WXY.V, version $Revision: 3.11 $, $Date: 2001/07/24 00:53:06 $
+#@(#) File WXY.V, version $Revision: 3.12 $, $Date: 2001/09/19 22:32:28 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package WXY of code WARP
@@ -9,7 +9,7 @@ wxy
 
 *********** WXYversion:
 # Quantities associated with version control 
-verswxy character*19 /"$Revision: 3.11 $"/ # Current code version, set by CVS
+verswxy character*19 /"$Revision: 3.12 $"/ # Current code version, set by CVS
 
 *********** Particlesxy dump parallel:
 npmaxxy    integer # Number of particles - same as npmax from TOP
