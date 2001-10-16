e99f27aa72c325177e866affbb9dd3408866f4ec
dave <dave>
2001-10-16 20:48:30 +0000

    Bug fix for getzmmnt for J90. The accumulation of zmmnts0 was not checking for dead particles.

--- pywarp90/top.v
+++ pywarp90/top.v
@@ -1,5 +1,5 @@
 top
-#@(#) File TOP.V, version $Revision: 3.23 $, $Date: 2001/09/20 00:50:30 $
+#@(#) File TOP.V, version $Revision: 3.24 $, $Date: 2001/10/16 20:48:31 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -617,7 +617,7 @@ linpgrd(0:npgrdol)        _logical         # Flag for when pgrd element in mesh
 
 *********** TOPversion:
 # Version control for global commons
-verstop character*19 /"$Revision: 3.23 $"/ # Global common version, set by CVS
+verstop character*19 /"$Revision: 3.24 $"/ # Global common version, set by CVS
 
 *********** Ctl_to_pic:
 # Communication between CTL and pic packages.  In TOP since it's "global"
