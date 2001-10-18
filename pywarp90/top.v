8c81a6f5333778aa16e663830e235a38b770ce66
dave <dave>
2001-10-18 20:27:34 +0000

    Generalized residence corrections somewhat by making the corrections independent of the direction of motion of the particles. Code had assumed that the particle was moving from left to right (increasing z). Now a particle can move from right to left (decreasing z). All of the elements which use residence corrections have been updated.

--- pywarp90/top.v
+++ pywarp90/top.v
@@ -1,5 +1,5 @@
 top
-#@(#) File TOP.V, version $Revision: 3.24 $, $Date: 2001/10/16 20:48:31 $
+#@(#) File TOP.V, version $Revision: 3.25 $, $Date: 2001/10/18 20:27:35 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -617,7 +617,7 @@ linpgrd(0:npgrdol)        _logical         # Flag for when pgrd element in mesh
 
 *********** TOPversion:
 # Version control for global commons
-verstop character*19 /"$Revision: 3.24 $"/ # Global common version, set by CVS
+verstop character*19 /"$Revision: 3.25 $"/ # Global common version, set by CVS
 
 *********** Ctl_to_pic:
 # Communication between CTL and pic packages.  In TOP since it's "global"
