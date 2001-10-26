3c5d40586c1efdf0781f3f29eb165b042468b17e
dave <dave>
2001-10-26 18:35:24 +0000

    Synchronize with f3d.F

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.14 $, $Date: 2001/10/19 22:06:17 $
+#@(#) File F3D.V, version $Revision: 3.15 $, $Date: 2001/10/26 18:35:24 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -9,7 +9,7 @@ f3d
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.14 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.15 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
