3f663395971652083182f46ea4015d6a8f0f62de
dave <dave>
2001-11-06 22:24:28 +0000

    The multipole component n=0, v=0, of emlt is now included in the envelope solution, providing an Ez on axis.

--- pywarp90/env.v
+++ pywarp90/env.v
@@ -1,5 +1,5 @@
 env
-#@(#) File ENV.V, version $Revision: 3.11 $, $Date: 2001/08/10 18:39:55 $
+#@(#) File ENV.V, version $Revision: 3.12 $, $Date: 2001/11/06 22:24:28 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package ENV of the WARP code.
@@ -13,7 +13,7 @@ env
 
 *********** ENVversion:
 # Version control for env package
-versenv character*19 /"$Revision: 3.11 $"/  # Current code version, set by CVS
+versenv character*19 /"$Revision: 3.12 $"/  # Current code version, set by CVS
 
 *********** ENVvars dump:
 # Variables needed by the package ENV
