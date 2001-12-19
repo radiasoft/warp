e462743473303df8154df21c7b821aa76e1834ec
dave <dave>
2001-12-19 22:11:39 +0000

    Fixed calculation of ix, iy, and iz_axis. Now uses nint and works for both negative and positive mmin's.

--- pywarp90/wxy.v
+++ pywarp90/wxy.v
@@ -1,5 +1,5 @@
 wxy
-#@(#) File WXY.V, version $Revision: 3.14 $, $Date: 2001/12/04 23:09:09 $
+#@(#) File WXY.V, version $Revision: 3.15 $, $Date: 2001/12/19 22:11:39 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package WXY of code WARP
@@ -9,7 +9,7 @@ wxy
 
 *********** WXYversion:
 # Quantities associated with version control 
-verswxy character*19 /"$Revision: 3.14 $"/ # Current code version, set by CVS
+verswxy character*19 /"$Revision: 3.15 $"/ # Current code version, set by CVS
 
 *********** Particlesxy dump parallel:
 npmaxxy    integer # Number of particles - same as npmax from TOP
