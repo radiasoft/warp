2e3cdb5e3e95f150b425bb5f1bdb4e4453224a78
dave <dave>
2002-04-03 23:08:15 +0000

    Minor change to call to diagnostics. When zbeaml and zbeamr are calculated (used to determine is various plots and other diagnostics are done), absolute values are taken so that zbeaml < zbeamr, even if dt < 0 or vbeamfrm < 0. This ensures that diagnostics will be taken if dt < 0 or vbeamfrm < 0, assuming that the diagnostic locations are set properly.

--- pywarp90/wxy.v
+++ pywarp90/wxy.v
@@ -1,5 +1,5 @@
 wxy
-#@(#) File WXY.V, version $Revision: 3.17 $, $Date: 2002/03/27 21:33:48 $
+#@(#) File WXY.V, version $Revision: 3.18 $, $Date: 2002/04/03 23:08:15 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package WXY of code WARP
@@ -9,7 +9,7 @@ wxy
 
 *********** WXYversion:
 # Quantities associated with version control 
-verswxy character*19 /"$Revision: 3.17 $"/ # Current code version, set by CVS
+verswxy character*19 /"$Revision: 3.18 $"/ # Current code version, set by CVS
 
 *********** Particlesxy dump parallel:
 npmaxxy    integer # Number of particles - same as npmax from TOP
