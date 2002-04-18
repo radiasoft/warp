c77a0b786195848e681b745cddd4e21de8cc4cd1
dave <dave>
2002-04-18 21:46:46 +0000

    Changed call to multigrid solver in vp3d to reflect changes to it.

--- pywarp90/w3d.v
+++ pywarp90/w3d.v
@@ -1,5 +1,5 @@
 w3d
-#@(#) File W3D.V, version $Revision: 3.45 $, $Date: 2002/04/03 23:09:27 $
+#@(#) File W3D.V, version $Revision: 3.46 $, $Date: 2002/04/18 21:46:46 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package W3D of code WARP
@@ -9,7 +9,7 @@ w3d
 
 *********** W3Dversion:
 # Quantities associated with version control 
-versw3d character*19 /"$Revision: 3.45 $"/ # Current code version, set by CVS
+versw3d character*19 /"$Revision: 3.46 $"/ # Current code version, set by CVS
 
 *********** InPltCtl3d dump:
 # Controls for when the various plots are made
