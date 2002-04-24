c5ff12d064ae59e6b2981043d5552aff9178855b
dave <dave>
2002-04-24 21:38:57 +0000

    Minor changes  - particles are considered dead when uzp == 0, rather than not >0.  - added abs's around inj_d in a number of places to get the code ready to inject    particles upstream.

--- pywarp90/w3d.v
+++ pywarp90/w3d.v
@@ -1,5 +1,5 @@
 w3d
-#@(#) File W3D.V, version $Revision: 3.52 $, $Date: 2002/04/23 14:10:43 $
+#@(#) File W3D.V, version $Revision: 3.53 $, $Date: 2002/04/24 21:38:57 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package W3D of code WARP
@@ -9,7 +9,7 @@ w3d
 
 *********** W3Dversion:
 # Quantities associated with version control 
-versw3d character*19 /"$Revision: 3.52 $"/ # Current code version, set by CVS
+versw3d character*19 /"$Revision: 3.53 $"/ # Current code version, set by CVS
 
 *********** InPltCtl3d dump:
 # Controls for when the various plots are made
