47feaa6475b856e6d05ba535cb1f959c5602c489
dave <dave>
2002-05-22 20:28:11 +0000

    Minor rewrite of pyrestore to do some clean up and reorganization. Also added varsuffix option, which adds a suffix to all of the variables read in.

--- scripts/pyBasis.py
+++ scripts/pyBasis.py
@@ -15,7 +15,7 @@ except ImportError:
 import __main__
 import sys
 import cPickle
-Basis_version = "$Id: pyBasis.py,v 1.21 2002/05/15 00:19:54 dave Exp $"
+Basis_version = "$Id: pyBasis.py,v 1.22 2002/05/22 20:28:11 dave Exp $"
 
 if sys.platform in ['sn960510','linux-i386','linux2']:
   true = -1
@@ -336,22 +336,25 @@ def pydumpold(fname,attr="dump",vars=[]):
 # in of python variables is put in a 'try' command to make it idiot proof.
 # More fancy foot work is done to get new variables read in into the
 # global dictionary.
-def pyrestore(filename=None,fname=None,verbose=0,skip=[]):
+def pyrestore(filename=None,fname=None,verbose=0,skip=[],varsuffix=None):
   """
 Restores all of the variables in the specified file.
   - filename: file to read in from (assumes PDB format)
   - verbose=0: When true, prints out the names of variables which are read in
   - skip=[]: list of variables to skip
+  - varsuffix: when set, all variables read in will be given the suffix
+               Note that fortran variables are then read into python vars
   """
   # --- The original had fname, but changed to filename to be consistent
   # --- with restart and dump.
   if filename is None: filename = fname
-  # --- Make sure a filename was inputted.
+  # --- Make sure a filename was input.
   assert filename is not None,"A filename must be specified"
   # --- open pdb file
   ff = PR.PR(filename)
   # --- Get a list of all of the variables in the file, loop over that list
   vlist = ff.inquire_ls()
+
   # --- vlist is looped over twice. The first time reads in all of the scalar
   # --- variables and the python variables. The second reads in the arrays.
   # --- This is done so that the integers which specify the dimensions of
@@ -359,37 +362,46 @@ Restores all of the variables in the specified file.
   # --- not having the integers read in first would cause problems
   # --- (so far only in the f90 version).
   for v in vlist:
-    # --- If v in the skip list, then continue
-    if v in skip:
+
+    # --- First, extract variable name
+    if len(v) > 4 and v[-4]  == '@': vname = v[:-4]
+    elif v[-7:] == '@pickle':        vname = v[:-7]
+    elif v[-7:] == '@global':        vname = v[:-7]
+    else:                            vname = v
+
+    # --- If variable in the skip list, then skip
+    if vname in skip or \
+       (len(v) > 4 and v[-4]=='@' and v[-3:]+'.'+v[:-4] in skip):
       if verbose: print "skipping "+v
       continue
+
+    # --- Add suffix to name is given.
+    # --- varsuffix is wrapped in str in case a nonstring was passed in.
+    if varsuffix is not None: vname = vname + str(varsuffix)
+
     # --- If the variable has the suffix '@pkg' then it is a warp variable.
-    if len(v) > 4 and v[-4]=='@':
-      # --- If v in the skip list, then continue
-      if v[:-4] in skip or v[-3:]+'.'+v[:-4] in skip:
-        if verbose: print "skipping "+v
-        continue
+    # --- If varsuffix is supplied, than this branch is skipped since
+    # --- data is read into python variables.
+    if len(v) > 4 and v[-4]=='@' and varsuffix is None:
+      vname = v[-3:]+'.'+v[:-4]
       try:
-        # --- get the package that the variable is in
-        # --- Array assignment is different than scalar assignment.
+        # --- On this pass, only assign to scalars.
         if type(ff.__getattr__(v)) != type(array([])):
           # --- Simple assignment is done for scalars, using the exec command
-          if verbose: print "reading in "+v[-3:]+"."+v[:-4]
-          #pkg.__setattr__(v[-3:],ff.__getattr__(v))
-          exec(v[-3:]+'.'+v[:-4]+'=ff.__getattr__(v)',
-               __main__.__dict__,locals())
+          if verbose: print "reading in "+vname
+          exec(vname+'=ff.__getattr__(v)',__main__.__dict__,locals())
       except:
         # --- The catches errors in cases where the variable is not an
         # --- actual warp variable, for example if it had been deleted
         # --- after the dump was originally made.
-        print "Warning: There was problem restoring %s"% (v[-3:]+'.'+v[:-4])
+        print "Warning: There was problem restoring %s"% (vname)
     elif v[-7:] == '@pickle':
       # --- Thses would be interpreter variables written to the file
       # --- as pickled objects. The data is unpickled and the variable
       # --- in put in the main dictionary.
       try:
-        if verbose: print "reading in python variable "+v[:-7]
-        __main__.__dict__[v[:-7]] = cPickle.loads(ff.__getattr__(v))
+        if verbose: print "reading in pickled variable "+v[:-7]
+        __main__.__dict__[vname] = cPickle.loads(ff.__getattr__(v))
       except:
         if verbose: print "error with variable "+v[:-7]
     elif v[-7:] == '@global':
@@ -397,42 +409,50 @@ Restores all of the variables in the specified file.
       # --- from Basis. A simple assignment is done and the variable
       # --- in put in the main dictionary.
       try:
-        if verbose: print "reading in python variable "+v[:-7]
-        __main__.__dict__[v[:-7]] = ff.__getattr__(v)
+        if verbose: print "reading in Basis variable "+v[:-7]
+        __main__.__dict__[vname] = ff.__getattr__(v)
       except:
         if verbose: print "error with variable "+v[:-7]
     elif v[-9:] == '@parallel':
-      # --- Don't read in variables with suffix @parallel
+      # --- Ignore variables with suffix @parallel
       pass
     else:
       # --- These would be interpreter variables written to the file
       # --- from python (or other sources). A simple assignment is done and
       # --- the variable in put in the main dictionary.
+      # --- Note that this branch is used for fortran variables when varsuffix
+      # --- is set.
       try:
         if verbose: print "reading in python variable "+v
-        __main__.__dict__[v] = ff.__getattr__(v)
+        __main__.__dict__[vname] = ff.__getattr__(v)
       except:
-        if verbose: print "error with variable "+v[:-7]
-  # --- Now loop again to read in the arrays.
-  for v in vlist:
-    # --- If v in the skip list, then continue
-    if v in skip:
-      if verbose: print "skipping "+v
-      continue
-    if len(v) > 4 and v[-4]=='@':
-      # --- If v in the skip list, then continue
-      if v[:-4] in skip or v[-3:]+'.'+v[:-4] in skip:
+        if verbose: print "error with variable "+v
+
+  # --- Now loop again to read in the arrays. This does not need to be done
+  # --- if varsuffix was specified since in that case everything has
+  # --- already been read in.
+  if varsuffix is None:
+    for v in vlist:
+      # --- First, extract variable name
+      if len(v) > 4 and v[-4]  == '@': vname = v[:-4]
+      elif v[-7:] == '@pickle':        vname = v[:-7]
+      elif v[-7:] == '@global':        vname = v[:-7]
+      else:                            vname = v
+      # --- If variable in the skip list, then skip
+      if vname in skip or \
+         (len(v) > 4 and v[-4]=='@' and v[-3:]+'.'+v[:-4] in skip):
         if verbose: print "skipping "+v
         continue
-      try:
-        pkg = eval(v[-3:],__main__.__dict__)
-        if type(ff.__getattr__(v)) == type(array([])):
-          # --- forceassign is used, allowing the array read in to have a
-          # --- different size than the current size of the warp array.
-          if verbose: print "reading in "+v[-3:]+"."+v[:-4]
-          pkg.forceassign(v[:-4],ff.__getattr__(v))
-      except:
-        print "Warning: There was problem restoring %s"% (v[-3:]+'.'+v[:-4])
+      if len(v) > 4 and v[-4]=='@':
+        try:
+          pkg = eval(v[-3:],__main__.__dict__)
+          if type(ff.__getattr__(v)) == type(array([])):
+            # --- forceassign is used, allowing the array read in to have a
+            # --- different size than the current size of the warp array.
+            if verbose: print "reading in "+v[-3:]+"."+v[:-4]
+            pkg.forceassign(v[:-4],ff.__getattr__(v))
+        except:
+          print "Warning: There was problem restoring %s"% (v[-3:]+'.'+v[:-4])
   ff.close()
 
 # --- create an alias for pyrestore
