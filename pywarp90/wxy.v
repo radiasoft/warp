3a64453a38a7ec981d0d9910f34a1f841b3072c3
dave <dave>
2002-06-04 17:20:19 +0000

    Minor bug fix for parallel version in wxygen. nz should only be set to nslaves on the first call to generate, when the parallel decomposition is done. Only affects cases when generate is called multiple times.

--- pywarp90/wxy.v
+++ pywarp90/wxy.v
@@ -1,5 +1,5 @@
 wxy
-#@(#) File WXY.V, version $Revision: 3.20 $, $Date: 2002/04/30 21:08:50 $
+#@(#) File WXY.V, version $Revision: 3.21 $, $Date: 2002/06/04 17:20:19 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package WXY of code WARP
@@ -9,7 +9,7 @@ wxy
 
 *********** WXYversion:
 # Quantities associated with version control 
-verswxy character*19 /"$Revision: 3.20 $"/ # Current code version, set by CVS
+verswxy character*19 /"$Revision: 3.21 $"/ # Current code version, set by CVS
 
 *********** Particlesxy dump parallel:
 npmaxxy    integer # Number of particles - same as npmax from TOP
