176fe1a0b52a06df5cce655ada4c6bd9cb030127
dave <dave>
2002-06-12 23:54:05 +0000

    Added code to set and use the new flag lhist.

--- pywarp90/wxy.v
+++ pywarp90/wxy.v
@@ -1,5 +1,5 @@
 wxy
-#@(#) File WXY.V, version $Revision: 3.21 $, $Date: 2002/06/04 17:20:19 $
+#@(#) File WXY.V, version $Revision: 3.22 $, $Date: 2002/06/12 23:54:21 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package WXY of code WARP
@@ -9,7 +9,7 @@ wxy
 
 *********** WXYversion:
 # Quantities associated with version control 
-verswxy character*19 /"$Revision: 3.21 $"/ # Current code version, set by CVS
+verswxy character*19 /"$Revision: 3.22 $"/ # Current code version, set by CVS
 
 *********** Particlesxy dump parallel:
 npmaxxy    integer # Number of particles - same as npmax from TOP
