299b0ae289a69f296f2d841483800a21ce8e257d
dave <dave>
2002-08-06 22:14:47 +0000

    Version synchronization

--- pywarp90/w3d.v
+++ pywarp90/w3d.v
@@ -1,5 +1,5 @@
 w3d
-#@(#) File W3D.V, version $Revision: 3.63 $, $Date: 2002/08/06 22:14:38 $
+#@(#) File W3D.V, version $Revision: 3.64 $, $Date: 2002/08/06 22:14:47 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package W3D of code WARP
@@ -9,7 +9,7 @@ w3d
 
 *********** W3Dversion:
 # Quantities associated with version control 
-versw3d character*19 /"$Revision: 3.63 $"/ # Current code version, set by CVS
+versw3d character*19 /"$Revision: 3.64 $"/ # Current code version, set by CVS
 
 *********** Obsolete3d:
 inj_d                real /0/ # Obsolete, now see inj_d in top
