f485e01c7633f860cceb0919fc3c02fd98553ddf
dave <dave>
2002-08-28 16:14:29 +0000

    Moved definition of Chombo field solver interface routines, like reachablenodes, to cho package.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.53 $, $Date: 2002/08/27 23:23:33 $
+#@(#) File F3D.V, version $Revision: 3.54 $, $Date: 2002/08/28 16:14:29 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -9,7 +9,7 @@ f3d
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.53 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.54 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -395,15 +395,6 @@ vcap3d   (iwhich,rho:real,phi:real,kxsq:real,kysq:real,kzsq:real,attx:real,
           nx,ny,nz,scrtch:real,xmmax:real,zmmin:real,zgrid:real,
           pipeshpe:string,periinz:logical,l2symtry:logical,l4symtry:logical)
      subroutine # External routine for capacity matrix field solve
-
-
-****** Chombosubs:
-reachablenodes(dx:real,mask:integer,xlo:integer,ylo:integer,zlo:integer,
-               xhi:integer,yhi:integer,zhi:integer,ncomp:integer) subroutine
-coverednodes(dx:real,mask:integer,xlo:integer,ylo:integer,zlo:integer,
-             xhi:integer,yhi:integer,zhi:integer) subroutine
-nodalcoefficients(dx:real,coeffs:real,xlo:integer,ylo:integer,zlo:integer,
-                  xhi:integer,yhi:integer,zhi:integer,ncomp:integer) subroutine
 
 ******** ConductorGeometryGenerators:
 setconductorparity(nn:integer,ix:integer,iy:integer,iz:integer,
