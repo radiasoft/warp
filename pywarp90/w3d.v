099dfff552473b6d6c24cb2bf5558a0746e5f857
dave <dave>
2002-09-04 18:10:31 +0000

    In inject3d, replaced code which removed dead particles at end of array with a call to cleardeadpart. Also, in loadrho3d, added call to cleardeadpart when the AMR/Chombo fieldsolver is being used.

--- pywarp90/w3d.v
+++ pywarp90/w3d.v
@@ -1,5 +1,5 @@
 w3d
-#@(#) File W3D.V, version $Revision: 3.70 $, $Date: 2002/08/29 17:54:15 $
+#@(#) File W3D.V, version $Revision: 3.71 $, $Date: 2002/09/04 18:10:31 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package W3D of code WARP
@@ -9,7 +9,7 @@ w3d
 
 *********** W3Dversion:
 # Quantities associated with version control 
-versw3d character*19 /"$Revision: 3.70 $"/ # Current code version, set by CVS
+versw3d character*19 /"$Revision: 3.71 $"/ # Current code version, set by CVS
 
 *********** Obsolete3d:
 inj_d                real /0/ # Obsolete, now see inj_d in top
