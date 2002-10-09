f23e1a2cbabbaaa52bfafab2ce1497f91212a76a
petermc <petermc>
2002-10-09 02:02:57 +0000

    Added returnphic subroutine.

--- pywarp90/cho.v
+++ pywarp90/cho.v
@@ -1,5 +1,5 @@
 cho
-#@(#) File CHO.V, version $Revision: 1.9 $, $Date: 2002/09/26 20:38:03 $
+#@(#) File CHO.V, version $Revision: 1.10 $, $Date: 2002/10/09 02:02:57 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package cho of code WARP6
@@ -9,7 +9,7 @@ cho
 }
 
 *********** CHOversion:
-verscho character*19 /"$Revision: 1.9 $"/#  Code version version is set by CVS
+verscho character*19 /"$Revision: 1.10 $"/#  Code version version is set by CVS
 
 *********** CHOHandle:
 cho_handle integer # Handle to the ChomboPIC package
@@ -30,6 +30,7 @@ cho_setamrgrids(nx:integer, ny:integer, nz:integer, dx:real, xmmin:real,
            bcxlo:integer, bcxhi:integer, bcylo:integer, bcyhi:integer,
            bczlo:integer, bczhi:integer) subroutine
 returnphi(phiout:real) subroutine
+returnphic(phiout:real) subroutine
 returnrho(rhoout:real) subroutine
 cho_solve3d(iwhich:integer,nx:integer,ny:integer,nz:integer,nzfull:integer,
             dx:real,dy:real,dz:real,l2symtry,l4symtry,
