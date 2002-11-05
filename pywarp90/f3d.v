f931c0560963ec279e3c3ccd2694e16215a8fd00
dave <dave>
2002-11-05 22:03:34 +0000

    Added mglevelspart, which is a flag for whether each level is partial or full coarsening. In parallel, setting mglevelspart is a global operation since due to roundoff problems, each processor can get a different result.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.61 $, $Date: 2002/11/01 00:21:19 $
+#@(#) File F3D.V, version $Revision: 3.62 $, $Date: 2002/11/05 22:03:34 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -9,7 +9,7 @@ f3d
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.61 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.62 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -229,6 +229,8 @@ mglevelsnz(0:100) integer     # List of nz for the levels of coarsening
 mglevelslx(0:100) real /101*1/ # List of coarsening factors in x
 mglevelsly(0:100) real /101*1/ # List of coarsening factors in y
 mglevelslz(0:100) real /101*1/ # List of coarsening factors in z
+mglevelspart(0:100) logical    # List of flags for whether full or partial
+                               # coarsening is done: 0 is full, 1 is partial
 mggoodnumbers(40) integer /4,6,8,10,12,14,16,20,24,28,32,40,48,56,64,80,96,112,
                            128,160,192,224,256,320,384,448,512,640,768,896,
                            1024,1280,1536,1792,2048,2560,3072,3584,5120,7168/
