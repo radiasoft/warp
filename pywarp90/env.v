48436070536140c646ca4ec13276ca283aa0822f
dave <dave>
2003-02-05 18:49:32 +0000

    A number of changes which allow dzenv to be negative, i.e. to run backwards. Note that the changes are not complete - the calculation of the phase advance needs to be fixed still.

--- pywarp90/env.v
+++ pywarp90/env.v
@@ -1,5 +1,5 @@
 env
-#@(#) File ENV.V, version $Revision: 3.15 $, $Date: 2002/04/09 18:52:45 $
+#@(#) File ENV.V, version $Revision: 3.16 $, $Date: 2003/02/05 18:49:32 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package ENV of the WARP code.
@@ -13,7 +13,7 @@ env
 
 *********** ENVversion:
 # Version control for env package
-versenv character*19 /"$Revision: 3.15 $"/  # Current code version, set by CVS
+versenv character*19 /"$Revision: 3.16 $"/  # Current code version, set by CVS
 
 *********** ENVvars dump:
 # Variables needed by the package ENV
