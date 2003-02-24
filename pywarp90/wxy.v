f0cc249f80af8704fba4998702abd139786bc2fb
dave <dave>
2003-02-24 19:04:25 +0000

    Synchronize with wxy.F.

--- pywarp90/wxy.v
+++ pywarp90/wxy.v
@@ -1,5 +1,5 @@
 wxy
-#@(#) File WXY.V, version $Revision: 3.28 $, $Date: 2003/02/24 19:03:45 $
+#@(#) File WXY.V, version $Revision: 3.29 $, $Date: 2003/02/24 19:04:25 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package WXY of code WARP
@@ -9,7 +9,7 @@ wxy
 
 *********** WXYversion:
 # Quantities associated with version control 
-verswxy character*19 /"$Revision: 3.28 $"/ # Current code version, set by CVS
+verswxy character*19 /"$Revision: 3.29 $"/ # Current code version, set by CVS
 
 *********** Particlesxy dump parallel:
 npmaxxy    integer # Number of particles - same as npmax from TOP
