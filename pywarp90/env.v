375bf9c334d72cf02b959e61cb5992a9f318b66b
dave <dave>
2003-03-04 03:04:59 +0000

    Added more tests to the code testing fortran derived types.

--- pywarp90/env.v
+++ pywarp90/env.v
@@ -1,5 +1,5 @@
 env
-#@(#) File ENV.V, version $Revision: 3.19 $, $Date: 2003/03/03 19:51:16 $
+#@(#) File ENV.V, version $Revision: 3.20 $, $Date: 2003/03/04 03:04:59 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package ENV of the WARP code.
@@ -13,7 +13,7 @@ env
 
 *********** ENVversion:
 # Version control for env package
-versenv character*19 /"$Revision: 3.19 $"/  # Current code version, set by CVS
+versenv character*19 /"$Revision: 3.20 $"/  # Current code version, set by CVS
 
 *********** ENVvars dump:
 # Variables needed by the package ENV
