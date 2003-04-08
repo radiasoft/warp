6aaf06376f3bf4d37e9970db370730b2c4bff22f
dave <dave>
2003-04-08 00:31:53 +0000

    Make small improvement to sendcommands - now control-d can be used to exit cleanly. Many improvements to documentation.

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -1,24 +1,70 @@
+"""
+Sets up a remote run monitor. To use, first create the monitor in the
+running job by inserting the following command somewhere in the input deck
+before the long running commands:
+
+createmonitor(passwd='mypassword')
+
+Then, from a remotely running python, import this module and run the
+following to create the connection
+
+connect(machine="machinename",passwd='mypassword')
+
+where "machinename" is the name of the machine where the original run
+is. Note that the password must be the same as in the createmonitor
+command. (The password provides very minimal security and is sent in
+plain text so do not use a sensitive password!) A "+++" prompt will be
+shown and commands typed will be sent to the remote job.
+
+When the "+++" prompt is displayed, the remote job will stop execution
+and wait for commands. To continue the job, type the control-d key.
+"""
 from warp import *
 import socket
 import time
 import re
-monitor_version = "$Id: monitor.py,v 1.1 2000/10/16 18:34:19 dave Exp $"
+monitor_version = "$Id: monitor.py,v 1.2 2003/04/08 00:31:53 dave Exp $"
 
-# --- Set up send and receive commands which first send the number of
-# --- bytes in the message. The random number 39487 is subtracted
-# --- from the number of bytes sent as a form of minimal security, making
-# --- it more difficult to hack into the running job.
 def socketsend(sock,s):
+  """
+Set up send command which first sends the number of bytes in the
+message. The random number 39487 is subtracted from the number of bytes
+sent as a form of minimal security, making it more difficult to hack
+into the running job.
+  """
   sock.send('%10d'%(len(s)-39487))
   sock.send(s)
 
 def socketrecv(sock):
+  """
+Set up receive command which first sends the number of bytes in the
+message. The random number 39487 is added from the number of bytes sent
+as a form of minimal security, making it more difficult to hack into the
+running job.
+  """
   nbytes = sock.recv(10)
   nbytes = eval(nbytes) + 39487
   return sock.recv(nbytes)
 
 # --- Create class for server side of run.
 class Monitor:
+  """
+Creates a monitor for a running job.
+ - port=50007: port number to use
+ - passwd='0': password to provide minimal security
+  """
+  def __init__(self,port=50007,passwd='0'):
+    self.passwd = passwd
+    self.port = port
+    self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    self.sock.bind(("",port))
+    self.sock.getsockname()
+    self.sock.listen(1)
+    self.sock.setblocking(0)
+    self.client = []
+    self.gettingcommands = 0
+    installafterfs(self.checksocket)
+    installafterstep(self.getcommands)
   def checksocket(self):
     """
 Check if anyone has opened the port. If so, get the socket and set up
@@ -100,20 +146,13 @@ and if so, get commands from it.
       top.maxcalls = self.maxcallssave
       top.ncall = self.ncallsave
       self.gettingcommands = 0
-  def __init__(self,port=50007,passwd='0'):
-    self.passwd = passwd
-    self.port = port
-    self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    self.sock.bind(("",port))
-    self.sock.getsockname()
-    self.sock.listen(1)
-    self.sock.setblocking(0)
-    self.client = []
-    self.gettingcommands = 0
-    installafterfs(self.checksocket)
-    installafterstep(self.getcommands)
 
 def createmonitor(port=50007,passwd='0'):
+  """
+Creates a monitor for a running job.
+ - port=50007: port number to use
+ - passwd='0': password to provide minimal security
+  """
   global _monitor
   _monitor = Monitor(port=port,passwd=passwd)
 
@@ -147,14 +186,28 @@ def sendcommands():
     try:
       s = raw_input('+++ ')
     except EOFError:
+      try:
+        sendeval('cont')
+      except socket.error:
+        pass
       break
-    if re.search('=',s):
-      sendexec(s)
-    else:
-      sendeval(s)
+    try:
+      if re.search('=',s):
+        sendexec(s)
+      else:
+        sendeval(s)
+    except socket.error:
+      break
 
 # --- Create functions for client side
-def connect(machine="ife1.lbl.gov",port=50007,passwd='0',auto=1): 
+def connect(machine="localhost",port=50007,passwd='0',auto=1): 
+  """
+Make a connection to a running job with a monitor.
+  - machine="localhost": machine on which job is running
+  - port=50007: port the running job is monitoring
+  - password='0': password the running job expects
+  - auto=1: when 1, go directly into the "+++" prompt
+  """
   global _sock
   _sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
   _sock.connect((machine,port))
