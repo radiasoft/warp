64f8c70b4945122ffaff29fb8cb6aea675cddd5b
dave <dave>
2003-04-28 19:17:19 +0000

    Bug fix in applypgrd. A line was added to try and check for lost particles. It used the wrong check and the wrong z index and was causing problems for the slice code. It would also cause problems in the 3-D code in cases when particles have z values less than zero. Now check for whether a particle in within the z-extent of the element compare z against the start and end of the element. I check on z/dz is also made. The same change was made to applybgrd.

--- pywarp90/top.v
+++ pywarp90/top.v
@@ -1,5 +1,5 @@
 top
-#@(#) File TOP.V, version $Revision: 3.84 $, $Date: 2003/04/22 22:26:33 $
+#@(#) File TOP.V, version $Revision: 3.85 $, $Date: 2003/04/28 19:17:20 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -61,7 +61,7 @@ codeid   character*8  /"warp r2"/     # Name of code, and major version
 
 *********** TOPversion:
 # Version control for global commons
-verstop character*19 /"$Revision: 3.84 $"/ # Global common version, set by CVS
+verstop character*19 /"$Revision: 3.85 $"/ # Global common version, set by CVS
 
 *********** Machine_param:
 wordsize integer /64/ # Wordsize on current machine--used in bas.wrp
