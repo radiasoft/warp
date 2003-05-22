e92599a20332cc2ef34fc313d0860078a07adfe2
dave <dave>
2003-05-22 00:13:15 +0000

    Bug fixes. Now works with Chombo!

--- pywarp90/cho.v
+++ pywarp90/cho.v
@@ -1,5 +1,5 @@
 cho
-#@(#) File CHO.V, version $Revision: 1.13 $, $Date: 2003/01/07 18:32:56 $
+#@(#) File CHO.V, version $Revision: 1.14 $, $Date: 2003/05/22 00:13:15 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package cho of code WARP6
@@ -9,19 +9,25 @@ cho
 }
 
 *********** CHOversion:
-verscho character*19 /"$Revision: 1.13 $"/#  Code version version is set by CVS
+verscho character*19 /"$Revision: 1.14 $"/#  Code version version is set by CVS
 
 *********** CHOHandle:
-cho_handle integer # Handle to the ChomboPIC package
-cho_status integer # Status from most recent call to ChomboPIC
+cho_handle integer /0/ # Handle to the ChomboPIC package
+cho_status integer /0/ # Status from most recent call to ChomboPIC
 cho_bunchid integer # Particle bunch id
 
 *********** CHOInput dump:
 cho_nlevels  integer /1/ # Number of levels in Chombo hierarchy
 cho_refratio integer /2/ # Refinement ratio
-cho_maxparticlespercell integer /0/ # Maximum particles per cell
+cho_maxparticlespercell integer /100000/ # Maximum particles per cell
+cho_tagsbuffercells integer /0/ # Depth of buffer around refined cells
+cho_fillratio real /0.8/ # Sets blockiness of refined regions, between 0 and 1
 cho_tol real /1.e-8/ # Tolerance on field solution
 cho_bcflags(3,2) integer /6*0/ # Boundary conditions. 0 Dirichlet, 1 Nuemman
+cho_bcvals(3,2) real /6*0./ # Boundary values for grid boundary faces
+cho_chargeinterptype integer /1/ # Charge interpolation type, 0 for NGP, 1 for
+                                 # linear
+cho_debug integer /1/ # Sets amount of debugging output and checking
 lcho_allnodesreachable logical /.true./ # When true, all nodes are considered reachable
 
 *********** CHOsubs:
