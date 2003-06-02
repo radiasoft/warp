90a2ba3fd799e8284b7220fe0aa7810e2e6e8446
dave <dave>
2003-06-02 17:44:16 +0000

    In initdtp, now only sets dtp for particles where uzp is nonzero.

--- pywarp90/wxy.v
+++ pywarp90/wxy.v
@@ -1,5 +1,5 @@
 wxy
-#@(#) File WXY.V, version $Revision: 3.31 $, $Date: 2003/04/22 21:50:49 $
+#@(#) File WXY.V, version $Revision: 3.32 $, $Date: 2003/06/02 17:44:16 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package WXY of code WARP
@@ -9,7 +9,7 @@ wxy
 
 *********** WXYversion:
 # Quantities associated with version control 
-verswxy character*19 /"$Revision: 3.31 $"/ # Current code version, set by CVS
+verswxy character*19 /"$Revision: 3.32 $"/ # Current code version, set by CVS
 
 *********** Particlesxy dump parallel:
 npmaxxy    integer # Number of particles - same as npmax from TOP
