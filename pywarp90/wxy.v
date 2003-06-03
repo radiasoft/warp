75b6de23b150dae88ba77598d1d9b5220c415d8b
dave <dave>
2003-06-03 23:06:44 +0000

    Changed wxygen to use the new initlabwn routine to setup the lab moments.

--- pywarp90/wxy.v
+++ pywarp90/wxy.v
@@ -1,5 +1,5 @@
 wxy
-#@(#) File WXY.V, version $Revision: 3.32 $, $Date: 2003/06/02 17:44:16 $
+#@(#) File WXY.V, version $Revision: 3.33 $, $Date: 2003/06/03 23:06:44 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package WXY of code WARP
@@ -9,7 +9,7 @@ wxy
 
 *********** WXYversion:
 # Quantities associated with version control 
-verswxy character*19 /"$Revision: 3.32 $"/ # Current code version, set by CVS
+verswxy character*19 /"$Revision: 3.33 $"/ # Current code version, set by CVS
 
 *********** Particlesxy dump parallel:
 npmaxxy    integer # Number of particles - same as npmax from TOP
