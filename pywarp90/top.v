74d7fa586980218a5ef085390f520fe84a6848ef
dave <dave>
2003-06-30 18:52:45 +0000

    Changed definition of rrms to be sqrt(xsqbar + ysqbar) instead of sqrt(xrms**2 + yrms**2), since the latter incorrectly shifts the moments by the beam offset.

--- pywarp90/top.v
+++ pywarp90/top.v
@@ -1,5 +1,5 @@
 top
-#@(#) File TOP.V, version $Revision: 3.94 $, $Date: 2003/06/27 23:16:45 $
+#@(#) File TOP.V, version $Revision: 3.95 $, $Date: 2003/06/30 18:52:45 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -60,7 +60,7 @@ codeid   character*8  /"warp r2"/     # Name of code, and major version
 
 *********** TOPversion:
 # Version control for global commons
-verstop character*19 /"$Revision: 3.94 $"/ # Global common version, set by CVS
+verstop character*19 /"$Revision: 3.95 $"/ # Global common version, set by CVS
 
 *********** Machine_param:
 wordsize integer /64/ # Wordsize on current machine--used in bas.wrp
