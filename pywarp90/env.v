c96ca2a9ecfd9803ee41d91e996280f4337dd759
dave <dave>
2003-06-30 16:58:41 +0000

    Implemented the solenoid field using mmlt with multipole indices 0,0.

--- pywarp90/env.v
+++ pywarp90/env.v
@@ -1,5 +1,5 @@
 env
-#@(#) File ENV.V, version $Revision: 3.22 $, $Date: 2003/06/02 17:43:26 $
+#@(#) File ENV.V, version $Revision: 3.23 $, $Date: 2003/06/30 16:58:41 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package ENV of the WARP code.
@@ -13,7 +13,7 @@ env
 
 *********** ENVversion:
 # Version control for env package
-versenv character*19 /"$Revision: 3.22 $"/  # Current code version, set by CVS
+versenv character*19 /"$Revision: 3.23 $"/  # Current code version, set by CVS
 
 *********** ENVvars dump:
 # Variables needed by the package ENV
