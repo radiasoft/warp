6ad10342225c6db568dca19c312f952d17d0ac80
dave <dave>
2003-07-16 18:07:11 +0000

    Bug fix in getzmmnt - line setting nep to zero needed to check whether nepwin was > 0 to ensure that nep was allocated.

--- pywarp90/top.v
+++ pywarp90/top.v
@@ -1,5 +1,5 @@
 top
-#@(#) File TOP.V, version $Revision: 3.97 $, $Date: 2003/07/16 00:32:06 $
+#@(#) File TOP.V, version $Revision: 3.98 $, $Date: 2003/07/16 18:07:11 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -60,7 +60,7 @@ codeid   character*8  /"warp r2"/     # Name of code, and major version
 
 *********** TOPversion:
 # Version control for global commons
-verstop character*19 /"$Revision: 3.97 $"/ # Global common version, set by CVS
+verstop character*19 /"$Revision: 3.98 $"/ # Global common version, set by CVS
 
 *********** Machine_param:
 wordsize integer /64/ # Wordsize on current machine--used in bas.wrp
