796499db8f636a506a87e36380a073ea65df8c6a
dave <dave>
2003-08-29 17:30:14 +0000

    Several changes:  - added check of lgetvzofz before call to getvzofz  - added call to alotlostpart in w3dgen  - added bound0 etc variables to calling sequences of field solvers

--- pywarp90/w3d.v
+++ pywarp90/w3d.v
@@ -1,5 +1,5 @@
 w3d
-#@(#) File W3D.V, version $Revision: 3.127 $, $Date: 2003/08/25 23:04:29 $
+#@(#) File W3D.V, version $Revision: 3.128 $, $Date: 2003/08/29 17:30:15 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package W3D of code WARP
@@ -9,7 +9,7 @@ w3d
 
 *********** W3Dversion:
 # Quantities associated with version control 
-versw3d character*19 /"$Revision: 3.127 $"/ # Current code version, set by CVS
+versw3d character*19 /"$Revision: 3.128 $"/ # Current code version, set by CVS
 
 *********** Obsolete3d:
 inj_d                real /0/ # Obsolete, now see inj_d in top
