7be29608cae83fd348a07059b046e30f8549e9cd
dave <dave>
2003-08-29 23:14:20 +0000

    Small optimization for extrapolated particles code in getzmmnt. Now if the izepwin is outside the grid, then the loop over particles is skipped.

--- pywarp90/top.v
+++ pywarp90/top.v
@@ -1,5 +1,5 @@
 top
-#@(#) File TOP.V, version $Revision: 3.104 $, $Date: 2003/08/29 17:28:25 $
+#@(#) File TOP.V, version $Revision: 3.105 $, $Date: 2003/08/29 23:14:20 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -60,7 +60,7 @@ codeid   character*8  /"warp r2"/     # Name of code, and major version
 
 *********** TOPversion:
 # Version control for global commons
-verstop character*19 /"$Revision: 3.104 $"/ # Global common version, set by CVS
+verstop character*19 /"$Revision: 3.105 $"/ # Global common version, set by CVS
 
 *********** Machine_param:
 wordsize integer /64/ # Wordsize on current machine--used in bas.wrp
