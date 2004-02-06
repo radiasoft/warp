2fc93e40505cec8302a508cac61f2a0ce1f2f73a
dave <dave>
2004-02-06 21:37:03 +0000

    Minor fix to impact_ions - now if the condid argument is zero, then it is ignored.

--- pywarp90/top.v
+++ pywarp90/top.v
@@ -1,5 +1,5 @@
 top
-#@(#) File TOP.V, version $Revision: 3.117 $, $Date: 2004/01/29 20:36:17 $
+#@(#) File TOP.V, version $Revision: 3.118 $, $Date: 2004/02/06 21:37:03 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -60,7 +60,7 @@ codeid   character*8  /"warp r2"/     # Name of code, and major version
 
 *********** TOPversion:
 # Version control for global commons
-verstop character*19 /"$Revision: 3.117 $"/ # Global common version, set by CVS
+verstop character*19 /"$Revision: 3.118 $"/ # Global common version, set by CVS
 
 *********** Machine_param:
 wordsize integer /64/ # Wordsize on current machine--used in bas.wrp
