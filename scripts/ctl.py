b302f91e2f5cb3d1104c25db03813b0a815a48ef
dave <dave>
2004-02-10 16:48:01 +0000

    Added code to handle interrupt requests (control-C). Now, a check is only made for an interrupt after all of a timestep is completed. The code is not complete since the fortran needs to know whether to start back up with a fullv or halfv.

--- scripts/ctl.py
+++ scripts/ctl.py
@@ -1,9 +1,43 @@
 # Control module
-ctl_version = "$Id: ctl.py,v 1.4 2003/09/23 19:38:42 dave Exp $"
+ctl_version = "$Id: ctl.py,v 1.5 2004/02/10 16:48:01 dave Exp $"
 from warp import *
+import signal
 
+#############################################################################
+# --- Setup signal handler to capture Control-C
+# --- When a interrupt request is received, all this handler does is set a
+# --- flag to that effect. Then, a subsequent call to ruthere will check
+# --- that flag, and if set, raise an exception. This allows a graceful
+# --- stop with the current time step completed.
+# --- Set the following two in case ruthere is called before setinterrupt.
+_defaultcontrolC = signal.getsignal(signal.SIGINT)
+_controlCrecieved = 0
+def _handlecontrolC(signum,frame):
+  global _controlCrecieved
+  _controlCrecieved = 1
+def ruthere():
+  """
+Checks if an interrupt was requested (usually control-C). If so, then raise
+an exception. This always restores the original interrupt handler so that the
+calling code does not have to, and so that, if there is an exception, it gets
+restored (since the calling code is not returned to).
+  """
+  global _controlCrecieved
+  oldsignal = signal.signal(signal.SIGINT,_defaultcontrolC)
+  if _controlCrecieved:
+    _controlCrecieved = 0
+    raise "Interrupt requested"
+def setinterrupt():
+  global _controlCrecieved,_defaultcontrolC
+  _controlCrecieved = 0
+  _defaultcontrolC = signal.signal(signal.SIGINT,_handlecontrolC)
+
+
+#############################################################################
 def generate():
   "Generates the current package"
+  setinterrupt()
+  _defaultcontrolC = signal.signal(signal.SIGINT,_handlecontrolC)
   for p in package():
     try:
       exec 'command = '+p+'gen'
@@ -16,6 +50,7 @@ def generate():
     #pass
   # --- Get generate time
   top.gentime = wtime() - top.starttime
+  ruthere()
 
 beforestepfuncs = []
 afterstepfuncs = []
@@ -27,12 +62,12 @@ def step(n=1,maxcalls=None):
       break
     except:
       pass
-  #try:
   if maxcalls is None: maxcalls = n
   top.maxcalls = maxcalls
   ncalls = n
   top.ncall = 0
   while top.ncall < ncalls:
+    setinterrupt()
     top.ncall = top.ncall + 1
 
     bb = wtime()
@@ -42,7 +77,6 @@ def step(n=1,maxcalls=None):
     except: step.beforetime = 0.
 
     command(1,1)
-    ruthere()
 
     bb = wtime()
     for f in afterstepfuncs: f()
@@ -54,13 +88,15 @@ def step(n=1,maxcalls=None):
     #top.steptime = wtime() - top.starttime - top.gentime
     # --- Flush the stdout buffer
     sys.stdout.flush()
-  #except:
-    #pass
+
+    ruthere()
+
   # --- Get step time
   a = wtime()
   top.steptime = top.steptime + (a - b)
 
 def finish():
+  setinterrupt()
   for p in package():
     try:
       exec 'command = '+p+'fin'
@@ -71,6 +107,7 @@ def finish():
     command(1,1)
   except:
     pass
+  ruthere()
 
 ########################################################################
 def stepz(zstep=0.):
