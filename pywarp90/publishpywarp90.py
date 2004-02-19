b01bdea2d68b9f518ad447d53e4dd02cb872cce4
dave <dave>
2004-02-19 00:27:26 +0000

    Used to maintain a public version of Warp on a machine. Typically, Warp will be built in one directory and then the binary installed in a separate directory. This script will automatically to that copy as well as update, using cvs, the scripts which are also in that public directory. This also appends the date to the executable so that older versions are still available. It also maintains a copy of the source in the public directory.

---
+++ pywarp90/publishpywarp90.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import os
+import sys
+import time
+t = time.localtime(time.time())
+date = "%s%02d%02d"%(t[0],int(t[1]),int(t[2]))
+
+if len(sys.argv) > 1:
+  pubhomes = sys.argv[1:]
+else:
+  pubhomes = ['$HOME/pub']
+
+def executecommand(s):
+  print s
+  e = os.system(s)
+
+for pubhome in pubhomes:
+  executecommand('cp pywarp90 '+pubhome+'/pywarp90'+date)
+  executecommand('cp warpC.so '+pubhome+'/warpC'+date+'.so')
+  executecommand('cd '+pubhome+';chmod go+rx pywarp90'+date+' warpC'+date+'.so')
+  executecommand('cd '+pubhome+';ln -sf pywarp90'+date+' pywarp90')
+  executecommand('cd '+pubhome+';ln -sf warpC'+date+'.so warpC.so')
+  executecommand('cd '+pubhome+'/source/pywarp90;cvs update')
+  executecommand('cd '+pubhome+'/scripts;cvs update')
