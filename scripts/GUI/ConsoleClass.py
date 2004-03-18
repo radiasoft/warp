cfdc0ca9aef6ee818733541f1badca25371c55cd
jlvay <jlvay>
2004-03-18 11:12:34 +0000

    Added autolayout for autoresizing with pareent; forces the console to be the first page in the notebook; fixed a few other things.

--- scripts/GUI/ConsoleClass.py
+++ scripts/GUI/ConsoleClass.py
@@ -1,6 +1,7 @@
 #Boa:FramePanel:ConsoleClass
 
 from wxPython.wx import *
+from wxPython.lib.anchors import LayoutAnchors
 from warp import *
 
 [wxID_CONSOLECLASS, wxID_CONSOLECLASSCONSOLE, 
@@ -18,17 +19,21 @@ class ConsoleClass(wxPanel):
               style=wxMAXIMIZE_BOX | wxTAB_TRAVERSAL)
         self._init_utils()
         self.SetClientSize(wxSize(596, 315))
+        self.SetAutoLayout(True)
 
         self.Console = wxTextCtrl(id=wxID_CONSOLECLASSCONSOLE, name='Console',
               parent=self, pos=wxPoint(0, 0), size=wxSize(596, 315),
               style=wxTE_MULTILINE, value='')
         self.Console.SetFont(wxFont(12, wxMODERN, wxNORMAL, wxNORMAL, false,''))
+        self.Console.SetConstraints(LayoutAnchors(self.Console, True, True,
+              True, True))
         EVT_CHAR(self.Console, self.OnconsoleChar)
         EVT_RIGHT_DOWN(self.Console, self.OnConsoleRightDown)
+        EVT_LEFT_UP(self.Console, self.OnConsoleLeftUp)
 
     def __init__(self, parent, inter):
         self._init_ctrls(parent)
-        parent.AddPage(imageId=-1, page=self, select=False, text='Console')
+        parent.InsertPage(index=0, imageId=-1, page=self, select=False, text='Console')
         self.listcommands=[]
         self.numcommand=-1
         self.CursorMin = 0
@@ -52,6 +57,7 @@ class ConsoleClass(wxPanel):
         else:
           self.Console.WriteText('... ')
         self.CursorMin = self.Console.GetLastPosition()
+        self.ins_point = self.CursorMin
        
     def sendcommand(self,c,addlist=1):
         r = self.inter.push(c)
@@ -72,6 +78,7 @@ class ConsoleClass(wxPanel):
             self.numcommand = max(self.numcommand-1,0)
             self.Console.Remove(self.CursorMin,self.Console.GetLastPosition())
             self.Console.WriteText(self.listcommands[self.numcommand])
+            self.Reset_ins_point()
         elif(ascii_code == 319): # arrow down
             self.numcommand = min(self.numcommand+1,len(self.listcommands))
             self.Console.Remove(self.CursorMin,self.Console.GetLastPosition())
@@ -79,33 +86,53 @@ class ConsoleClass(wxPanel):
               self.Console.WriteText(self.listcommands[self.numcommand])
             else:
               self.Console.WriteText(self.LineBuffer)
+            self.Reset_ins_point()
         elif(ascii_code == 316): # arrow left
-            self.ins_point = self.Console.GetInsertionPoint()
+            self.Set_ins_point()
             self.Console.SetInsertionPoint(max(self.ins_point-1,self.CursorMin))
+            self.ins_point -= 1
         elif(ascii_code == 318): # arrow right
-            self.ins_point = self.Console.GetInsertionPoint()
+            self.Set_ins_point()
             self.Console.SetInsertionPoint(min(self.ins_point+1,self.Console.GetLastPosition()))
+            self.ins_point += 1
         elif(ascii_code == 8): # backspace
-            self.ins_point = self.Console.GetInsertionPoint()
+            self.Set_ins_point()
             rmv_point = max(self.ins_point-1,self.CursorMin)
             self.Console.Remove(rmv_point,rmv_point+1)
+            self.ins_point -= 1
         elif(ascii_code == 127): # delete
-            self.ins_point = self.Console.GetInsertionPoint()
+            self.Set_ins_point()
             rmv_point = min(self.ins_point,self.Console.GetLastPosition())
             self.Console.Remove(rmv_point,rmv_point+1)
         elif(ascii_code == 13): # return
             self.Console.SetInsertionPoint(self.Console.GetLastPosition())
             self.send_command_line()
         elif(ascii_code>=32 and ascii_code<255): # character       
-            self.ins_point=max(self.ins_point,self.CursorMin)
+            self.Set_ins_point()
             self.Console.SetInsertionPoint(self.ins_point)
             self.Console.WriteText(chr(ascii_code))
             self.ins_point+=1
         else:
             pass
 
-    def OnConsoleRightDown(self, event):
-        self.GetParent().GetParent().OutToMessageWindow()
-        print doc(self.Console.GetStringSelection(),printit=0)
-        self.GetParent().GetParent().OutToConsole()
+    def Set_ins_point(self):
+        newins_point = self.Console.GetInsertionPoint()
+        if(self.ins_point<>newins_point and newins_point>=self.CursorMin):
+           self.ins_point = newins_point
+        
+    def Reset_ins_point(self):
+        self.ins_point = self.Console.GetInsertionPoint()
+        
+    def OnConsoleLeftUp(self, event):
+        newins_point = self.Console.GetInsertionPoint()
+        if(self.ins_point<>newins_point and newins_point>=self.CursorMin):
+           self.ins_point = newins_point
         event.Skip()
+
+    def OnConsoleRightDown(self, event):
+        self.GetParent().GetParent().GetParent().OutToMessageWindow()
+        print doc(self.Console.GetStringSelection(),printit=0)
+        self.GetParent().GetParent().GetParent().OutToConsole()
+        event.Skip()
+
+
