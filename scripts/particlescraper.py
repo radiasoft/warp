3857b3496cd7b3a8b8651fa397998981c3415a4d
dave <dave>
2004-03-31 14:18:23 +0000

    Add the option of replacing lost particle position data with the location where the particles intercepted the surface of the conductor they were lost on. Also cleaned up the code, removing the use of top.pid as temporary arrays. Now, temp arrays are just created dynamically.

--- scripts/particlescraper.py
+++ scripts/particlescraper.py
@@ -4,7 +4,7 @@ ParticleScraper: class for creating particle scraping
 from warp import *
 from generateconductors import *
 
-particlescraper_version = "$Id: particlescraper.py,v 1.12 2004/03/23 15:47:49 dave Exp $"
+particlescraper_version = "$Id: particlescraper.py,v 1.13 2004/03/31 14:18:23 dave Exp $"
 def particlescraperdoc():
   import particlescraper
   print particlescraper.__doc__
@@ -17,47 +17,33 @@ Class for creating particle scraper for conductors
                Note that each conductor MUST have a unique id.
  - lsavecondid: when true, the id of the conductor where the particle is
                 lost is save. The id is saved in the array top.pidlost[:,-1].
+ - lsaveintercept: when true, the location and angle where the particle
+                   intercepted the conductor surface is calculated. The
+                   location is overwritten onto the xplost, yplost, and zplost
+                   arrays. The angle is put into pidlost[:,-2].
+                   Note that the condid where the particle is lost is also
+                   saved in pidlost[:,-1].
  - install=1: flag whether or not to install the scraper so that the scraping
               automatically happens every time step.
 After an instance is created, additional conductors can be added by calling
 the method registerconductors which takes either a conductor or a list of
 conductors are an argument.
   """
-  _scrapeid = None
-  def __init__(self,conductors,lsavecondid=0,install=1): 
+  def __init__(self,conductors,lsavecondid=0,lsaveintercept=0,install=1): 
     # --- Create grid
     self.grid = Grid()
     # --- register any initial conductors
     self.conductors = []
     self.registerconductors(conductors)
-    # --- Add extra column(s) to top.pid for work space. Only do this if this
-    # --- is the first time an instance of this class is created or if the
-    # --- the extra column(s) had been removed. Otherwise use the already
-    # --- allocated column(s) in top.pid.
-    if (ParticleScraper._scrapeid is None or
-        top.npid-1 < ParticleScraper._scrapeid or
-        (w3d.solvergeom != w3d.XYZgeom and
-         top.npid-1 < ParticleScraper._scrapeid+1)):
-      ParticleScraper._scrapeid = top.npid
-      self.npid = top.npid
-      top.npid = top.npid + 1
-      if w3d.solvergeom != w3d.XYZgeom:
-        self.pwork = top.npid
-        top.npid = top.npid + 1
-      top.npidmax = max(top.npidmax,top.npid)
-      top.npidlostmax = max(top.npidlostmax,top.npidmax)
-    else:
-      self.npid = ParticleScraper._scrapeid
-      if w3d.solvergeom != w3d.XYZgeom:
-        self.pwork = ParticleScraper._scrapeid + 1
-    # --- Make sure that npmaxi is set
-    top.npmaxi = max(top.npmax,2)
-    gchange("Particles")
+    ## --- Make sure that npmaxi is set
+    #top.npmaxi = max(top.npmax,2)
+    #gchange("Particles")
     gchange("LostParticles")
     # --- If the conductor id where particles are lost is being saved,
     # --- need to turn on saving of lost particles.
-    self.lsavecondid = lsavecondid
-    if lsavecondid:
+    self.lsaveintercept = lsaveintercept
+    self.lsavecondid = lsavecondid or lsaveintercept
+    if self.lsavecondid:
       top.lsavelostpart = true
     # --- Install the call to scrape particles if requested
     if install: self.installscraper()
@@ -98,35 +84,28 @@ conductors are an argument.
     # --- First, find any particles near a conductor
     i1 = top.ins[js] - 1
     i2 = top.ins[js] + top.nps[js] - 1
-    top.pid[i1:i2,self.npid] = 0.
     xx = top.xp[i1:i2]
     yy = top.yp[i1:i2]
     zz = top.zp[i1:i2]
+    pp = zeros(top.nps[js],'d')
     if w3d.solvergeom == w3d.XYZgeom:
-      getgrid3d(top.nps[js],xx,yy,zz,top.pid[i1:i2,self.npid],
+      getgrid3d(top.nps[js],xx,yy,zz,pp,
                 nx,ny,nz,isinside,xmin,xmax,ymin,ymax,zmin,zmax,
                 w3d.l2symtry,w3d.l4symtry)
     else:
-      top.pid[i1:i2,self.pwork] = sqrt(xx**2 + yy**2)
-      xx = top.pid[i1:i2,self.pwork]
+      xx = sqrt(xx**2 + yy**2)
       yy = zeros(len(xx),'d')
-      getgrid2d(top.nps[js],xx,zz,top.pid[i1:i2,self.npid],
-                nx,nz,isinside,xmin,xmax,zmin,zmax)
-    iscrape = compress(top.pid[i1:i2,self.npid]>0.,arange(i1,i2))
+      getgrid2d(top.nps[js],xx,zz,pp,nx,nz,isinside,xmin,xmax,zmin,zmax)
+
+    iscrape = compress(pp>0.,arange(i1,i2))
     if len(iscrape) == 0: return
 
     # --- Duplicate the particle list eight times, once for each corner.
-    # --- The direction in which the corners lie depends on the symmetry
-    # --- and the sign of the coordinate.
     iscrape = repeat(iscrape,8)
     nn = len(iscrape)
     xx = take(xx,iscrape-i1)
     yy = take(yy,iscrape-i1)
     zz = take(zz,iscrape-i1)
-    if w3d.l4symtry: sx = sign(ones(nn),xx)
-    else: sx = 1.
-    if w3d.l4symtry or w3d.l2symtry: sy = sign(ones(nn),yy)
-    else: sy = 1.
     xg = xmin+int(abs(xx-xmin)/dx)*dx + array(nn/8*[0.,dx,0.,dx,0.,dx,0.,dx])
     yg = ymin+int(abs(yy-ymin)/dy)*dy + array(nn/8*[0.,0.,dy,dy,0.,0.,dy,dy])
     zg = zmin+int(abs(zz-zmin)/dz)*dz + array(nn/8*[0.,0.,0.,0.,dz,dz,dz,dz])
@@ -155,9 +134,15 @@ conductors are an argument.
 
 
   def savecondid(self,js):
+
+    # --- Just return if there are no lost particles.
+    if top.npslost[js] == 0: return
+
     # --- First make sure there is extra space in the pidlost array.
-    if top.npidlostmax < top.npidmax+1:
-      top.npidlostmax = top.npidmax + 1
+    pidspace = 1
+    if self.lsaveintercept: pidspace = 2
+    if top.npidlostmax < top.npidmax+pidspace:
+      top.npidlostmax = top.npidmax + pidspace
       gchange("LostParticles")
 
     # --- Much of this code is duplicated from scrape above so if it changes,
@@ -182,25 +167,23 @@ conductors are an argument.
     yy = top.yplost[i1:i2]
     zz = top.zplost[i1:i2]
 
+    if w3d.solvergeom == w3d.RZgeom:
+      xx = sqrt(xx**2 + yy**2)
+      yy = zeros(len(xx),'d')
+
     # --- Get the indices of all lost particles that havn't been localized
     # --- to a conductor.
     iscrape = compress(top.pidlost[i1:i2,-1]==0,arange(i1,i2))
 
     # --- Duplicate the particle list eight times, once for each corner.
-    # --- The direction in which the corners lie depends on the symmetry
-    # --- and the sign of the coordinate.
     iscrape = repeat(iscrape,8)
     nn = len(iscrape)
-    xx = take(xx,iscrape-i1)
-    yy = take(yy,iscrape-i1)
-    zz = take(zz,iscrape-i1)
-    if w3d.l4symtry: sx = sign(ones(nn),xx)
-    else: sx = 1.
-    if w3d.l4symtry or w3d.l2symtry: sy = sign(ones(nn),yy)
-    else: sy = 1.
-    xg = xmin+int(abs(xx-xmin)/dx)*dx + array(nn/8*[0.,dx,0.,dx,0.,dx,0.,dx])
-    yg = ymin+int(abs(yy-ymin)/dy)*dy + array(nn/8*[0.,0.,dy,dy,0.,0.,dy,dy])
-    zg = zmin+int(abs(zz-zmin)/dz)*dz + array(nn/8*[0.,0.,0.,0.,dz,dz,dz,dz])
+    x8 = take(xx,iscrape-i1)
+    y8 = take(yy,iscrape-i1)
+    z8 = take(zz,iscrape-i1)
+    xg = xmin+int(abs(x8-xmin)/dx)*dx + array(nn/8*[0.,dx,0.,dx,0.,dx,0.,dx])
+    yg = ymin+int(abs(y8-ymin)/dy)*dy + array(nn/8*[0.,0.,dy,dy,0.,0.,dy,dy])
+    zg = zmin+int(abs(z8-zmin)/dz)*dz + array(nn/8*[0.,0.,0.,0.,dz,dz,dz,dz])
     pp = zeros(nn,'d')
 
     # --- Get conductor id that particles are near
@@ -215,12 +198,27 @@ conductors are an argument.
     for c in self.conductors:
       ii = compress(pp == c.condid,arange(nn))
       if len(ii) == 0: continue
-      xc = take(xx,ii)
-      yc = take(yy,ii)
-      zc = take(zz,ii)
+      xc = take(x8,ii)
+      yc = take(y8,ii)
+      zc = take(z8,ii)
       ic = take(iscrape,ii)
       ic = compress(c.isinside(xc,yc,zc).isinside,ic)
       # --- For particles which are inside, set pid to the id of the conductor
       # --- where the particle is lost.
       put(top.pidlost[:,-1],ic,c.condid)
+      # --- Save location and angle where particle intercepted the conductor.
+      if self.lsaveintercept:
+        xc = take(xx,ic-i1)
+        yc = take(yy,ic-i1)
+        zc = take(zz,ic-i1)
+        # --- Should really multiply by gaminv
+        vx = take(top.uxplost[i1:i2],ic-i1)
+        vy = take(top.uyplost[i1:i2],ic-i1)
+        vz = take(top.uzplost[i1:i2],ic-i1)
+        intercept = c.intercept(xc,yc,zc,vx,vy,vz)
+        put(top.xplost,ic,intercept.xi)
+        put(top.yplost,ic,intercept.yi)
+        put(top.zplost,ic,intercept.zi)
+        put(top.pidlost[:,-2],ic,intercept.angle)
+
 
