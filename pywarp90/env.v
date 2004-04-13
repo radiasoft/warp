3484ac1be0112cf0cddf5a299d6284cf982dbe43
dave <dave>
2004-04-13 22:22:25 +0000

    Bug fix in envelope code - the sign of the heleep component for n and v == 0 was incorrect.

--- pywarp90/env.v
+++ pywarp90/env.v
@@ -1,5 +1,5 @@
 env
-#@(#) File ENV.V, version $Revision: 3.29 $, $Date: 2004/03/11 15:33:09 $
+#@(#) File ENV.V, version $Revision: 3.30 $, $Date: 2004/04/13 22:22:25 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package ENV of the WARP code.
@@ -13,7 +13,7 @@ env
 
 *********** ENVversion:
 # Version control for env package
-versenv character*19 /"$Revision: 3.29 $"/  # Current code version, set by CVS
+versenv character*19 /"$Revision: 3.30 $"/  # Current code version, set by CVS
 
 *********** ENVvars dump:
 # Variables needed by the package ENV
