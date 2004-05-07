49316e771ac6cf15b4a6c4fe8dd440b8bd2c20d0
dave <dave>
2004-05-07 18:33:47 +0000

    Changes to vpois3d. Now, the choice between the longitudinal solvers is based on the boundary conditions. So if dirichlet boundary conditions are specified, the tridiagonal solver is used. Periodic uses the periodic FFT's. The 3d capacity matrix solvers (those that don't operate in kz space) can now use the tridiagonal solver.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.105 $, $Date: 2004/05/06 17:40:56 $
+#@(#) File F3D.V, version $Revision: 3.106 $, $Date: 2004/05/07 18:33:47 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.105 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.106 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
