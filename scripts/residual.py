090a8f452a1a99a33dd4c0e11b7a121a41901ddb
dave <dave>
2004-06-01 20:13:31 +0000

    Rewrote, converting into a function that takes the phi and rho as arguments.

--- scripts/residual.py
+++ scripts/residual.py
@@ -1,19 +1,28 @@
 # Calculate the residual of Poisson's equation.  The residual has the units
 # of Q/M**3 (same units as rho)
-residual_version = "$Id: residual.py,v 1.2 2000/12/01 01:34:57 dave Exp $"
+from warp import *
+residual_version = "$Id: residual.py,v 1.3 2004/06/01 20:13:31 dave Exp $"
 
-residual = zeros((w3d.nx+1,w3d.ny+1,w3d.nz+1),"d")
+def getresidual(phi=None,rho=None,dx=None,dy=None,dz=None):
+  if phi is None: phi = w3d.phi
+  if rho is None: rho = w3d.rho
+  if dx is None: dx = w3d.dx
+  if dy is None: dy = w3d.dy
+  if dz is None: dz = w3d.dz
+  residual = zeros(shape(rho),"d")
 
-residual[1:w3d.nx,1:w3d.ny,1:w3d.nz] = (top.eps0*(
-   (w3d.phi[0:w3d.nx-1,1:w3d.ny  ,2:w3d.nz+1] -
- 2.*w3d.phi[1:w3d.nx  ,1:w3d.ny  ,2:w3d.nz+1] +
-    w3d.phi[2:        ,1:w3d.ny  ,2:w3d.nz+1])/w3d.dx**2 +
-   (w3d.phi[1:w3d.nx  ,0:w3d.ny-1,2:w3d.nz+1] -
- 2.*w3d.phi[1:w3d.nx  ,1:w3d.ny  ,2:w3d.nz+1] +
-    w3d.phi[1:w3d.nx  ,2:        ,2:w3d.nz+1])/w3d.dy**2 +
-   (w3d.phi[1:w3d.nx  ,1:w3d.ny  ,1:w3d.nz  ] -
- 2.*w3d.phi[1:w3d.nx  ,1:w3d.ny  ,2:w3d.nz+1] +
-    w3d.phi[1:w3d.nx  ,1:w3d.ny  ,3:w3d.nz+2])/w3d.dz**2)+
-    w3d.rho[1:w3d.nx  ,1:w3d.ny  ,1:w3d.nz  ])
+  residual[1:-1,1:-1,1:-1] = (eps0*(
+     (phi[0:-2,1:-1,2:-2] -
+   2.*phi[1:-1,1:-1,2:-2] +
+      phi[2:  ,1:-1,2:-2])/dx**2 +
+     (phi[1:-1,0:-2,2:-2] -
+   2.*phi[1:-1,1:-1,2:-2] +
+      phi[1:-1,2:  ,2:-2])/dy**2 +
+     (phi[1:-1,1:-1,1:-3] -
+   2.*phi[1:-1,1:-1,2:-2] +
+      phi[1:-1,1:-1,3:-1])/dz**2)+
+      rho[1:-1,1:-1,1:-1])
+
+  return residual
 
 #print "Maximum residual = %e" % max(abs(ravel(residual)))
