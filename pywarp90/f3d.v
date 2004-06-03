f00fbf7cb32437a374633096cf54f9d36af0eb3c
dave <dave>
2004-06-03 21:55:51 +0000

    Minor fixes to setcndctr3d and srfrv_f. In both cases, protected if statements where out of bounds references to arrays may happen. This is a problem with fortran - it evaluates all parts of an if expression even though the value can be determined by partial evaluation.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.108 $, $Date: 2004/05/27 23:07:33 $
+#@(#) File F3D.V, version $Revision: 3.109 $, $Date: 2004/06/03 21:55:52 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.108 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.109 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
