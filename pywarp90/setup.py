bf06d3b06eaf06cbe5454b56caf524c9379475c5
dave <dave>
2004-07-22 23:20:20 +0000

    Added references to the new files w3d_injection.F and w3d_utilities.F.

--- pywarp90/setup.py
+++ pywarp90/setup.py
@@ -50,7 +50,8 @@ for pkg in warppkgs:
   warpobjects = warpobjects + makeobjects(pkg)
 
 warpobjects = warpobjects + ['dtop.o',
-                             'dw3d.o','w3d_interp.o',
+                             'dw3d.o','w3d_injection.o','w3d_interp.o',
+                             'w3d_utilities.o',
                              'f3d_mgrid.o','f3d_conductors.o','fft.o','util.o',
                              'fxy_mgrid.o',
                              'dwrz.o',
