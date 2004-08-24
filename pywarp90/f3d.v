2ad2af597ff800adee71e9b89e3e64b506774b65
dave <dave>
2004-08-24 00:09:54 +0000

    Added new subtimers group and included timers for two routines.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.113 $, $Date: 2004/08/13 22:20:25 $
+#@(#) File F3D.V, version $Revision: 3.114 $, $Date: 2004/08/24 00:09:54 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.113 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.114 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -692,3 +692,9 @@ getconductorsnewfacet(ix:integer,iy:integer,iz:integer,oo:integer,
                       dels:real,gridmin:real,griddd:real,pp:real,npp:integer)
                       subroutine
 conductorsmoothshading() subroutine
+
+******** Subtimersf3d:
+lf3dtimesubs logical /.false./
+timemultigrid3dsolve real /0./
+timegatherrhofromchild real /0./
+
