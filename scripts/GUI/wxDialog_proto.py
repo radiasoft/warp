cbe92bc7468ddc1d3bdca7ff8e621f5a06f7b9c0
jlvay <jlvay>
2004-12-07 22:26:32 +0000

    Added the possibility to create either a dialog or a frame.

--- scripts/GUI/wxDialog_proto.py
+++ scripts/GUI/wxDialog_proto.py
@@ -21,6 +21,7 @@ class wxDialog1(wxDialog):
               style=wxDEFAULT_DIALOG_STYLE, title='wxDialog1')
         self._init_utils()
         self.SetClientSize(wxSize(630, 410))
+        EVT_CLOSE(self, self.OnWxdialog1Close)
 
         self.window1 = wxWindow(id=wxID_WXDIALOG1WINDOW1, name='window1',
               parent=self, pos=wxPoint(0, 24), size=wxSize(640, 416), style=0)
@@ -47,5 +48,71 @@ class wxDialog1(wxDialog):
         self.parent.notebook1.GetPage(self.nbselection).Show(1)
         self.parent.notebook1.SetSelection(self.nbselection)
         self.panel.Move(wxPoint(0,0))
+#	self.panel.SetSize(self.parent.notebook1.GetSize())
+        self.panel.Refresh()
         self.Destroy()
+        event.Skip()
+
+    def OnWxdialog1Close(self, event):
+        self.OnTonotebookButton(event)
+        event.Skip()
+
+
+class wxFrame1(wxFrame):
+    def _init_utils(self):
+        # generated method, don't edit
+        pass
+
+    def _init_ctrls(self, prnt):
+        # generated method, don't edit
+        wxFrame.__init__(self, id=wxID_WXDIALOG1, name='', parent=prnt,
+              pos=wxPoint(419, 165), size=wxSize(638, 434),
+              style=wxDEFAULT_FRAME_STYLE, title='wxDialog1')
+        self._init_utils()
+        self.SetClientSize(wxSize(630, 410))
+    	self.SetAutoLayout(True)
+        EVT_CLOSE(self, self.OnWxframe1Close)
+
+        self.window1 = wxWindow(id=wxID_WXDIALOG1WINDOW1, name='window1',
+              parent=self, pos=wxPoint(0, 24), size=wxSize(640, 416), style=0)
+
+        self.tonotebook = wxButton(id=wxID_WXDIALOG1TONOTEBOOK,
+              label='to notebook', name='tonotebook', parent=self,
+              pos=wxPoint(0, 0), size=wxSize(75, 23), style=0)
+        self.tonotebook.SetBackgroundColour(wxColour(128, 128, 128))
+        self.tonotebook.SetForegroundColour(wxColour(255, 255, 255))
+        EVT_BUTTON(self.tonotebook, wxID_WXDIALOG1TONOTEBOOK,
+              self.OnTonotebookButton)
+
+    def __init__(self, parent, child, title):
+        self._init_ctrls(parent)
+        self.SetTitle(title)
+        self.parent = parent
+        self.title = title
+        if child is None: return
+        self.panel = child(self.window1)
+        self.child = child
+
+    def OnTonotebookButton(self, event):
+        self.panel.Reparent(self.parent.notebook1.GetPage(self.nbselection))
+        self.parent.notebook1.GetPage(self.nbselection).Show(1)
+        self.parent.notebook1.SetSelection(self.nbselection)
+        self.panel.Move(wxPoint(0,0))
+        cs = wxLayoutConstraints()
+        ref=self.parent.notebook1.GetPage(self.nbselection)
+        cs.top.SameAs(ref,wxTop)
+        cs.bottom.SameAs(ref,wxBottom)
+        cs.left.SameAs(ref,wxLeft)
+        cs.right.SameAs(ref,wxRight)
+        self.panel.SetConstraints(cs)
+        self.panel.SetAutoLayout(True)
+        self.panel.SetSize(self.parent.notebook1.GetSize())
+        self.parent.Refresh()
+        self.panel.Refresh()
+        self.parent.notebook1.Update()
+        self.Destroy()
+        event.Skip()
+
+    def OnWxframe1Close(self, event):
+        self.OnTonotebookButton(event)
         event.Skip()
