82d6a146f72fd3b7a102c9e9907653291e69051c
dave <dave>
2005-03-07 23:10:32 +0000

    Added method 2 which uses Jin-Soo's Newton method to include the electrons.

--- scripts/boltzmannelectrons.py
+++ scripts/boltzmannelectrons.py
@@ -1,13 +1,23 @@
 from warp import *
 
 class BoltzmanSolver:
-  def __init__(self,rhoion,te,phi0,n=1,method=0):
+  """
+ - rhoion: ion number density in the plasma
+ - vion: velocity if the ions
+         This is not used yet, but will be to control the injected particles
+ - te: electron temperature in volts
+ - phi0: plasma potential
+  """
+  def __init__(self,rhoion,vion,te,phi0,n=1,method=2):
     self.rhoion = rhoion
+    self.vion = vion
     self.te = te
     self.phi0 = phi0
     self.n = n
     self.method = method
-    if self.method == 1:
+    if self.method == 2:
+      installafterfs(self.solve2)
+    elif self.method == 1:
       self.rhoelectron = zeros(shape(w3d.rho),'d')
       self.phiprev = zeros(shape(w3d.rho),'d')
       self.parity = ones(shape(w3d.rho))
@@ -16,10 +26,31 @@ class BoltzmanSolver:
       self.parity[0::2,1::2,1::2] = 0
       self.parity[1::2,0::2,1::2] = 0
       installafterfs(self.solve1)
-    else:
-      installafterfs(self.solve)
+    elif self.method == 0:
+      installafterfs(self.solve0)
 
-  def solve(self):
+  def solve2(self):
+    phi = w3d.phi[:,:,2:-1]  # --- This is the updated phi with no electrons
+
+    rdx2 = 1./w3d.dx**2
+    rdy2 = 1./w3d.dy**2
+    rdz2 = 1./w3d.dz**2
+    const = 0.5/(1./w3d.dx**2 + 1./w3d.dy**2 + 1./w3d.dz**2)
+
+    # --- Treat electron term implicitly by the Newton's Method
+    # --- (one iteration is enough)
+    Gijk = self.rhoion*(const/eps0)
+    numer = Gijk*exp((phi - self.phi0)/self.te)
+    denom = 1.0 + numer/self.te
+    phinew = phi - numer/denom
+    phi[:,:,:] = where(phi-self.phi0 > -10*self.te,phinew,phi)
+
+    print "/home/kim/testwarp/boltzmannelectrons_jsk.py"
+    print "PotI,PotE:",maxnd(phi),maxnd(self.rhoelectron/eps0*const)
+    print "DnsI,DnsE:",maxnd(w3d.rho),maxnd(self.rhoelectron)
+
+
+  def solve0(self):
     for i in range(self.n):
       f3d.sormaxit = 1
       fieldsol(-1)
@@ -54,3 +85,4 @@ class BoltzmanSolver:
         print "DnsI,DnsE:",maxnd(w3d.rho),maxnd(self.rhoelectron)
         w3d.phi[:,:,0] = w3d.phi[:,:,1]
   
+
