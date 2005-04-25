d9c5e56f0eefe96e1d2d51855a3a91fba66e2781
dave <dave>
2005-04-25 23:28:34 +0000

    Added the variable isfsapi which is used to save the species that is being handled in fetche. This is to be used by python level field solvers.

--- pywarp90/w3d.v
+++ pywarp90/w3d.v
@@ -1,5 +1,5 @@
 w3d
-#@(#) File W3D.V, version $Revision: 3.185 $, $Date: 2005/03/17 18:07:28 $
+#@(#) File W3D.V, version $Revision: 3.186 $, $Date: 2005/04/25 23:28:34 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package W3D of code WARP
@@ -9,7 +9,7 @@ w3d
 
 *********** W3Dversion:
 # Quantities associated with version control 
-versw3d character*19 /"$Revision: 3.185 $"/ # Current code version, set by CVS
+versw3d character*19 /"$Revision: 3.186 $"/ # Current code version, set by CVS
 
 *********** Obsolete3d:
 inj_d                real /0/ # Obsolete, now see inj_d in top
@@ -333,6 +333,7 @@ selfe(3,0:nx_selfe,0:ny_selfe,0:nz_selfe) _real [V/m] # Self E field,
  # calculated from phi via finite difference. Only used when top.efetch = 3
 
 *********** FieldSolveAPI:
+isfsapi integer
 exfsapi(:) _real
 eyfsapi(:) _real
 ezfsapi(:) _real
