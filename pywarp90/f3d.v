47617af9312b8fdefe7f7be8e609d49e29f990ca
dave <dave>
2005-04-25 23:28:48 +0000

    Fixed the argument list to vpois3d.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.122 $, $Date: 2005/04/05 16:54:24 $
+#@(#) File F3D.V, version $Revision: 3.123 $, $Date: 2005/04/25 23:28:48 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.122 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.123 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -397,7 +397,8 @@ vpois3d  (iwhich:integer,a:real,ak:real,kxsq:real,kysq:real,kzsq:real,
          lx:real,ly:real,lz:real,nx:integer,ny:integer,nz:integer,
          nzfull:integer,
          w:real,xywork:real,zwork:real,ibc:integer,
-         l2symtry:logical,l4symtry:logical)
+         l2symtry:logical,l4symtry:logical,
+         bound0:integer,boundnz:integer,boundxy:integer)
      subroutine #  The 3d Poisson solver
 vp3x(iwhich) subroutine
      # Python-level interface to VPOIS3d, using FS3 database variables
