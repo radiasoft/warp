38167df459df25a4380be11ad5d49c7ec1db2675
dave <dave>
2005-06-22 00:48:57 +0000

    Removed the BEtemp group, which was only there for some debugging.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.126 $, $Date: 2005/06/17 23:47:33 $
+#@(#) File F3D.V, version $Revision: 3.127 $, $Date: 2005/06/22 00:48:57 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.126 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.127 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -726,8 +726,4 @@ timemgexchange_phi         real /0./
 timemgexchange_phiperiodic real /0./
 timemgexchange_rho         real /0./
 timeprintarray3d           real /0./
-
-****** BEtemp:
-uuu(:,:,:) _real
-vvv(:,:,:) _real
 
