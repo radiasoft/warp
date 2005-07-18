dfbfb71b5fd87b5ecef1a1678f26870056c7e2c0
jlvay <jlvay>
2005-07-18 23:42:29 +0000

    Fixed a few int*4 issues for mpi calls.

--- pywarp90/frzslave.F90
+++ pywarp90/frzslave.F90
@@ -14,7 +14,7 @@ contains
     INTEGER(ISZ), INTENT(IN) :: TID, tag
     REAL(8), DIMENSION(:) :: r
 !    WRITE(0,*) 'send to ',tid
-      call mpi_send(r,SIZE(r),mpi_double_precision,int(tid,4),int(tag,4),MPI_COMM_WORLD,ierr)
+      call mpi_send(r,int(SIZE(r),4),mpi_double_precision,int(tid,4),int(tag,4),MPI_COMM_WORLD,ierr)
 !    WRITE(0,*) 'sent to ',tid
   END SUBROUTINE mpi_send_real_array
 
@@ -35,7 +35,7 @@ contains
     REAL(8), DIMENSION(:) :: r
 
 !    WRITE(0,*) 'send to ',tid
-      call mpi_isend(r,SIZE(r),mpi_double_precision,int(tid,4),int(tag,4),MPI_COMM_WORLD,rqu,ierr)
+      call mpi_isend(r,int(SIZE(r),4),mpi_double_precision,int(tid,4),int(tag,4),MPI_COMM_WORLD,rqu,ierr)
 !    WRITE(0,*) 'sent to ',tid
   END SUBROUTINE mpi_isend_real_array
 
@@ -53,7 +53,7 @@ contains
   function mpi_global_compute_real(DATA,op)
     implicit none
     REAL(8) :: DATA, mpi_global_compute_real
-    INTEGER(ISZ) :: op
+    INTEGER(4) :: op
 !    WRITE(0,*) 'enter mpi_allreduce '
       call mpi_allreduce(data,mpi_global_compute_real,1_4,mpi_double_precision,int(op,4),mpi_comm_world,ierr)
 !    WRITE(0,*) 'exit mpi_allreduce '
