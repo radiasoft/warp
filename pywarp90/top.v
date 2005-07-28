1fc877e7d5904512c50651cfd4e1c192e4c4e42a
jlvay <jlvay>
2005-07-28 21:37:35 +0000

    Added variables for control of self magnetic field.

--- pywarp90/top.v
+++ pywarp90/top.v
@@ -1,5 +1,5 @@
 top
-#@(#) File TOP.V, version $Revision: 3.147 $, $Date: 2005/07/12 19:32:20 $
+#@(#) File TOP.V, version $Revision: 3.148 $, $Date: 2005/07/28 21:37:35 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -60,7 +60,7 @@ codeid   character*8  /"warp r2"/     # Name of code, and major version
 
 *********** TOPversion:
 # Version control for global commons
-verstop character*19 /"$Revision: 3.147 $"/ # Global common version, set by CVS
+verstop character*19 /"$Revision: 3.148 $"/ # Global common version, set by CVS
 
 *********** Machine_param:
 wordsize integer /64/ # Wordsize on current machine--used in bas.wrp
@@ -1939,6 +1939,8 @@ ins(ns)  _integer /1/  # Index of first particle in species
 nps(ns)  _integer /0/  # Number of particles in species
 ndts(ns) _integer /1/  -parallel # Stride for time step advance for each species
 ldts(ns) _logical  /1/ -parallel
+lselfb(ns) _logical 
+fselfb(ns) _real   /0./
 dtscale(ns) _real /1./ -parallel # Scale factor applied to time step size for
                                  # each species. Only makes sense in steady
                                  # state and transverse slice modes.
