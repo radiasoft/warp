919dfd107b1a5e8016a2814b109161864c3ed78a
dave <dave>
2005-09-13 20:34:59 +0000

    Added logical flag lusevectorpotential to the BField type. It defaults to true, which means that the vector potential is solved for, solving del^2 A = -mu0 J. Other, the B fields are solved for directly, solving del^2 B = -mu0 curl J. The latter is more correct since the former will incorrectly include any longitudinal parts of J.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.132 $, $Date: 2005/09/13 00:53:11 $
+#@(#) File F3D.V, version $Revision: 3.133 $, $Date: 2005/09/13 20:34:59 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.132 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.133 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -359,6 +359,9 @@ bounds(0:5) integer  # Boundary conditions on grid surfaces
 lcylindrical logical /.false./ # When true, signifies that cylindrical
                                # coordinates are being used, which means
                                # that 0 is r, 1 is theta, and 2 is z.
+lusevectorpotential logical /.true./ # When true, the vector potential A is
+                                      # solver for, solving del^2 A = -mu0 J.
+                                      # Otherwise solve del^2 B = -mu0 curl J.
 
 j(0:2,0:nx,0:ny,0:nz) _real # Current density
 b(0:2,0:nx,0:ny,0:nz) _real # B field, calculated from B = del cross A
