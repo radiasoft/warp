d754471ca9e4ded47a2093cc1d28d67e768a6b80
dave <dave>
2005-09-20 20:28:52 +0000

    Converted setupsiblingdomain and expandsiblingdomain back to plain fortran routines so that the code is dependent of new Forthon features.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.135 $, $Date: 2005/09/20 19:07:10 $
+#@(#) File F3D.V, version $Revision: 3.136 $, $Date: 2005/09/20 20:28:52 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.135 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.136 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -445,9 +445,9 @@ gatherrhofromchild(rho:real,nx:integer,ny:integer,nz:integer,
                    dobounds:integer,bounds:integer,rootdims:integer)
       subroutine
 setupsiblingdomain(nn:integer,sd:integer,od:integer)
-      csubroutine
+      subroutine
 expandsiblingdomain(nn:integer,d:integer,r:integer)
-      csubroutine
+      subroutine
 
 *********** Surface_of_Rev dump:
 srfrv_pernz              integer /0/ # Number of points per nz for tablized data
