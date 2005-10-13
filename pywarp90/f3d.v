f2e147322c2a9722171e511a6dc49d4ed3f26503
dave <dave>
2005-10-13 00:23:07 +0000

    Fixed the argument list of multigridbe3dsolve.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.139 $, $Date: 2005/10/10 17:01:52 $
+#@(#) File F3D.V, version $Revision: 3.140 $, $Date: 2005/10/13 00:23:07 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.139 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.140 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -297,8 +297,8 @@ multigridbe3dsolve(iwhich:integer,
              dx:real,dy:real,dz:real,phi:real,rho:real,
              rstar:real,linbend:logical,bounds:integer,
              xmmin:real,ymmin:real,zmmin:real,zbeam:real,zgrid:real,
-             mgparam:real,mgform:integer,mgiters:integer,
-             mgmaxiters:integer,mgmaxlevels:integer,mgerror:real,mgtol:real,
+             mgparam:real,mgiters:integer,mgmaxiters:integer,
+             mgmaxlevels:integer,mgerror:real,mgtol:real,
              downpasses:integer,uppasses:integer,
              lcndbndy:logical,laddconductor:logical,icndbndy:integer,
              lbuildquads:logical,gridmode:integer,conductors:ConductorType,
