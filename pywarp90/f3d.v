686b7bcf750486798a61c117824a22079acdecab
dave <dave>
2006-01-19 19:49:23 +0000

    Added the subroutine gatherphifromparents which is used by the 3D MR field solver.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.145 $, $Date: 2006/01/17 05:27:41 $
+#@(#) File F3D.V, version $Revision: 3.146 $, $Date: 2006/01/19 19:49:23 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.145 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.146 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -495,6 +495,9 @@ gatherrhofromchild(rho:real,nn:integer,childrho:real,cnn:integer,
                    r:integer,weights:real,
                    dobounds:integer,bounds:integer,rootdims:integer)
       subroutine
+gatherphifromparents(phi:real,nn:integer,l:integer,u:integer,fulllower:integer,
+                     parentphi:real,pnn:integer,parentlower:integer,r:integer)
+      subroutine
 
 *********** Surface_of_Rev dump:
 srfrv_pernz              integer /0/ # Number of points per nz for tablized data
@@ -890,6 +893,7 @@ conductorsmoothshading() subroutine
 lf3dtimesubs logical /.false./
 timemultigrid3dsolve real /0./
 timegatherrhofromchild real /0./
+timegatherphifromparents real /0./
 
 timeexchange_phi           real /0./
 timetranspose              real /0./
