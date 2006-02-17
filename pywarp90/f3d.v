244173abc2aadd8d511a4b6028736e1ba21a3c69
dave <dave>
2006-02-17 23:51:09 +0000

    Converted the loadj3d routine to use pgroup, which is now an input argument.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.147 $, $Date: 2006/01/31 01:31:19 $
+#@(#) File F3D.V, version $Revision: 3.148 $, $Date: 2006/02/17 23:51:09 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.147 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.148 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -457,7 +457,7 @@ bfield BFieldGridType
 bfieldp BFieldGridType
 init_bfieldsolver(bfstype:integer) subroutine # Initializes the B-field solver
 bfieldsol3d(iwhich) subroutine # Self B-field solver
-loadj3d(ins:integer,nps:integer,is:integer,lzero:logical) 
+loadj3d(ins:integer,nps:integer,is:integer,lzero:logical,pgroup:ParticleGroup) 
              subroutine # Provides a simple interface to the current density
                         # loading routine setj3d
 setaboundaries3d(bfield:BFieldGridType)
