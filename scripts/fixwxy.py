d176511241d5698c9526341848a6196eed74b63a
dave <dave>
2006-04-28 00:43:06 +0000

    Now uses pgroup, which is an input option.

--- scripts/fixwxy.py
+++ scripts/fixwxy.py
@@ -1,11 +1,11 @@
 """Fixes beam so that it exactly agress with the specified beam paramters
 """
 from warp import *
-fixwxy_version = "$Id: fixwxy.py,v 1.8 2005/05/27 22:40:03 dave Exp $"
+fixwxy_version = "$Id: fixwxy.py,v 1.9 2006/04/28 00:43:06 dave Exp $"
 
 # --- Fixes 1st and 2nd moments
 def fixwxy2(a=None,b=None,ap=None,bp=None,x=None,y=None,xp=None,yp=None, 
-            emitx=None,emity=None):
+            emitx=None,emity=None,pgroup=None):
   if a  is None: a  = top.a0
   if ap is None: ap = top.ap0
   if b  is None: b  = top.b0
@@ -14,6 +14,7 @@ def fixwxy2(a=None,b=None,ap=None,bp=None,x=None,y=None,xp=None,yp=None,
   if xp is None: xp = top.xp0
   if y  is None: y  = top.y0
   if yp is None: yp = top.yp0
+  if pgroup is None: pgroup = top.pgroup
   # --- x-emittance 
   if emitx is None: 
     if top.emitn == 0. and top.emitnx == 0.:
@@ -47,32 +48,42 @@ def fixwxy2(a=None,b=None,ap=None,bp=None,x=None,y=None,xp=None,yp=None,
   vy = yp*top.vbeam
 
   # --- Fix the beam center and size to match exactly to the input
-  top.xp[:] = where(not_equal(top.uzp, 0.),top.xp - top.xbar[0,-1] + x,0.)
-  top.yp[:] = where(not_equal(top.uzp, 0.),top.yp - top.ybar[0,-1] + y,0.)
-  top.xp[:] = top.xp*a/(2.*top.xrms[0,-1])
-  top.yp[:] = top.yp*b/(2.*top.yrms[0,-1])
+  pgroup.xp[:] = where(not_equal(pgroup.uzp, 0.),
+                       pgroup.xp - top.xbar[0,-1] + x,0.)
+  pgroup.yp[:] = where(not_equal(pgroup.uzp, 0.),
+                       pgroup.yp - top.ybar[0,-1] + y,0.)
+  pgroup.xp[:] = pgroup.xp*a/(2.*top.xrms[0,-1])
+  pgroup.yp[:] = pgroup.yp*b/(2.*top.yrms[0,-1])
 
   # --- Fix velocity to match emittance and envelope angle
   # --- First, remove any average velocity
-  if top.lrelativ: gi = top.gaminv
+  if top.lrelativ: gi = pgroup.gaminv
   else:            gi = 1.
-  top.uxp[:] = where(not_equal(top.uzp, 0.),top.uxp - top.vxbar[0,-1]/gi,0.)
-  top.uyp[:] = where(not_equal(top.uzp, 0.),top.uyp - top.vybar[0,-1]/gi,0.)
+  pgroup.uxp[:] = where(not_equal(pgroup.uzp, 0.),
+                        pgroup.uxp - top.vxbar[0,-1]/gi,0.)
+  pgroup.uyp[:] = where(not_equal(pgroup.uzp, 0.),
+                        pgroup.uyp - top.vybar[0,-1]/gi,0.)
   # --- Then remove any coherent velocity
   top.xxpbar[0] = ave(getx()*getxp())
   top.yypbar[0] = ave(gety()*getyp())
   slopex = top.xxpbar[0,-1]/(a/2.)**2*top.vbeam
   slopey = top.yypbar[0,-1]/(b/2.)**2*top.vbeam
-  top.uxp[:] = where(not_equal(top.uzp, 0.),top.uxp - slopex*top.xp,0.)
-  top.uyp[:] = where(not_equal(top.uzp, 0.),top.uyp - slopey*top.yp,0.)
+  pgroup.uxp[:] = where(not_equal(pgroup.uzp, 0.),
+                       pgroup.uxp - slopex*pgroup.xp,0.)
+  pgroup.uyp[:] = where(not_equal(pgroup.uzp, 0.),
+                       pgroup.uyp - slopey*pgroup.yp,0.)
   # --- Scale to get correct thermal spread
-  top.uxp[:] = top.uxp[:]*(emitx/a)/(top.epsx[0,-1]/(2.*top.xrms[0,-1]))
-  top.uyp[:] = top.uyp[:]*(emity/b)/(top.epsy[0,-1]/(2.*top.yrms[0,-1]))
+  pgroup.uxp[:] = (pgroup.uxp[:]*(emitx/a)/
+                   (top.epsx[0,-1]/(2.*top.xrms[0,-1])))
+  pgroup.uyp[:] = (pgroup.uyp[:]*(emity/b)/(
+                   top.epsy[0,-1]/(2.*top.yrms[0,-1])))
   # --- Added back in specified coherent velocity and average
   slopex = ap/a*top.vbeam
   slopey = bp/b*top.vbeam
-  top.uxp[:]=where(not_equal(top.uzp,0.),top.uxp+slopex*top.xp+vx,0.)
-  top.uyp[:]=where(not_equal(top.uzp,0.),top.uyp+slopey*top.yp+vy,0.)
+  pgroup.uxp[:]=where(not_equal(pgroup.uzp,0.),
+                      pgroup.uxp+slopex*pgroup.xp+vx,0.)
+  pgroup.uyp[:]=where(not_equal(pgroup.uzp,0.),
+                      pgroup.uyp+slopey*pgroup.yp+vy,0.)
 
   # --- Now fix up rho and diagnostics
   w3d.rho = 0.
@@ -87,11 +98,12 @@ def fixwxy2(a=None,b=None,ap=None,bp=None,x=None,y=None,xp=None,yp=None,
 
 # --- Fixes only 1st moments (beam centroid)
 def fixwxy1(x=None,y=None,xp=None,yp=None,xerr=0.,yerr=0.,xperr=0.,yperr=0.,
-            replacehist=1):
+            replacehist=1,pgroup=None):
   if x  is None: x  = top.x0
   if xp is None: xp = top.xp0
   if y  is None: y  = top.y0
   if yp is None: yp = top.yp0
+  if pgroup is None: pgroup = top.pgroup
 
   rr = sqrt(ranf())
   tt = ranf()*2.*pi
@@ -103,12 +115,16 @@ def fixwxy1(x=None,y=None,xp=None,yp=None,xerr=0.,yerr=0.,xperr=0.,yperr=0.,
   vy = (yp + yperr*rr*sin(tt))*top.vbeam
 
   # --- Fix the beam center to match exactly to the input
-  top.xp[:] = where(not_equal(top.uzp, 0.),top.xp - top.xbar[0,-1] + xx,0.)
-  top.yp[:] = where(not_equal(top.uzp, 0.),top.yp - top.ybar[0,-1] + yy,0.)
+  pgroup.xp[:] = where(not_equal(pgroup.uzp, 0.),
+                       pgroup.xp - top.xbar[0,-1] + xx,0.)
+  pgroup.yp[:] = where(not_equal(pgroup.uzp, 0.),
+                       pgroup.yp - top.ybar[0,-1] + yy,0.)
 
   # --- Fix velocity to match envelope angle
-  top.uxp[:] = where(not_equal(top.uzp, 0.),top.uxp - top.vxbar[0,-1] + vx,0.)
-  top.uyp[:] = where(not_equal(top.uzp, 0.),top.uyp - top.vybar[0,-1] + vy,0.)
+  pgroup.uxp[:] = where(not_equal(pgroup.uzp, 0.),
+                        pgroup.uxp - top.vxbar[0,-1] + vx,0.)
+  pgroup.uyp[:] = where(not_equal(pgroup.uzp, 0.),
+                        pgroup.uyp - top.vybar[0,-1] + vy,0.)
 
   # --- Now fix up rho and diagnostics
   w3d.rho = 0.
