51b4969187df0e0695288742a133be54751a77cd
jlvay <jlvay>
2006-07-28 15:35:05 +0000

    Removed unnecessary modules and added other things.

--- pywarp90/em2d.v
+++ pywarp90/em2d.v
@@ -1,5 +1,5 @@
 em2d
-#@(#) File EM2D.V, version $Revision: 1.5 $, $Date: 2006/07/25 17:31:19 $
+#@(#) File EM2D.V, version $Revision: 1.6 $, $Date: 2006/07/28 15:35:05 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -32,86 +32,6 @@ bndbxbyez        _type_bnd
 bndbxbyezc       _type_bnd
 bndbxbyezf       _type_bnd
 
-*********** EM2D_system dump:
-mksa                         integer /0/
-oneoverk0                    integer /1/
-inputunits                   integer /0/
-idebe                        integer /1/
-idebi                        integer /1/
-ifine                        integer /0/
-ifini                        integer /0/
-nbpart                       integer /0/
-nsteps                       integer /1/
-itime                        integer /0/
-dtfact                         real /0.995/
-delta_t                        real /0./
-tmin_moving_main_window      real /0./
-anormr	                     real /1./
-anormj                       real /1./
-xlong                          real /1./
-ylong                          real /1./
-lambda_laser                   real /1./ 
-xgauss                         real /1./
-ampfac                         real /1./
-nmaxw                        integer /1/
-ntamp_scatter                integer /2/
-ntamp_gather                 integer /4/
-xlbound                      integer /0/
-xrbound                      integer /0/
-ylbound                      integer /0/
-yrbound                      integer /0/
-pulse_type                   character*8 /"user_def"/
-stretch                      real    /1./
-misme                        real    /1836./
-tiste                        real    /0.1/
-vte                          real    /0./
-nstepe                       integer /1/
-xvide                        real    /3./
-npcell_max                   integer /1/
-tlance                       real    /1./
-teta                         real    /0./
-ksmth                        logical /.false./
-rho1                         real    /0./ 
-rho2                         real    /0./ 
-rho3                         real    /0./ 
-rho4                         real    /0./ 
-rho5                         real    /0./ 
-rhomax                       real    /0./ 
-xrh1                         real    /0./ 
-xrh2                         real    /0./ 
-xrh3                         real    /0./ 
-xrh4                         real    /0./ 
-xrh5                         real    /0./ 
-yrh1                         real    /0./ 
-yrh2                         real    /0./ 
-angle                        real    /0./
-cyl                          logical /.false./
-xcentre                      real    /0./
-ycentre                      real    /0./
-iboundary                    integer /1/
-proba_cool                   real    /0./
-vte_cool                     real    /0./
-trapeze                      logical /.false./
-esirkepov                    logical /.true./
-cyl_cool                     logical /.false./
-cyl_cool_centre              logical /.false./
-del_collect                  real    /0./
-icone                        integer /0/
-l_residue                    logical /.false./
-type_residue                 integer /1/
-l_verbose                    logical /.false./
-l_readinputnamelists         logical /.false./
-l_getfieldfrombase           logical /.false./
-l_absorbx                    logical /.true./
-l_absorby                    logical /.true./
-l_add_particles              logical /.false./
-l_add_particles_random       logical /.false./
-l_particles_weight           logical /.false./
-l_conserve_global_neutrality logical /.false./ # electrons are reflected at bnd if no ion has left
-l_linear_new                 logical /.true./
-l_overcycle_ions             logical /.true./
-l_push_ions                  logical /.true./
-
 *********** EM2D_FIELDobjects dump:
 field        _EM2D_FIELDtype
 fpatchcoarse _EM2D_FIELDtype
@@ -121,11 +41,16 @@ l_elaser_out_plane           logical /.false./
 l_moving_window              logical /.false./
 l_noinputfield               logical /.false./
 l_copyfields                 logical /.false./
-ixpatch integer
-iypatch integer
+l_smoothdensity              logical /.false./
 ntamp_apr                    integer /4/
 rap                          integer /1/
 ndelta_t                     integer /1/
+nxpatch                      integer /1/
+nypatch                      integer /1/
+ixpatch                      integer /0/
+iypatch                      integer /0/
+ntamp_scatter                integer /2/
+ntamp_gather                 integer /4/
 xminpatch_scatter            real /0./
 xmaxpatch_scatter            real /0./
 yminpatch_scatter            real /0./
@@ -144,32 +69,12 @@ Bxfsum(0:nxfsum+3,0:nyfsum+2) _real
 Byfsum(0:nxfsum+3,0:nyfsum+2) _real
 Bzfsum(0:nxfsum+3,0:nyfsum+2) _real
 
-*********** EM2D_main dump:
-# Variables needed by the main routines of package EMI
-nxmain integer /0/
-nymain integer /0/
-nplanmain integer /0/
-rapm integer /0/
-nxpload integer /0/
-nypload integer /0/
-npload(0:nxpload,0:nypload) _real 
-xminpload real /0./
-xmaxpload real /0./
-yminpload real /0./
-ymaxpload real /0./
-isave_freq integer /100/
-l_savefields logical /.false./
-l_noacceleration logical /.false./
-l_nodeposition logical /.false./
-datapath character*(80) /" "/
-rhoaux(0:nxmain+3,0:nymain+2,nplanmain) _real
-
 init_fields(f:EM2D_FIELDtype, nx:integer, ny:integer, 
            nbndx:integer, nbndy:integer, 
-           dtm:real, dx:real, dy:real, xmin:real, 
-           ymin:real, rap:integer, 
+           dtm:real, dx:real, dy:real, clight:real, mu0:real, 
+           xmin:real, ymin:real, rap:integer, 
            xlb:integer,ylb:integer,xrb:integer,yrb:integer) subroutine
-push_em_e(f:EM2D_FIELDtype,dt:real) subroutine
+push_em_e(f:EM2D_FIELDtype,dt:real,clight:real,mu0:real) subroutine
 push_em_b(f:EM2D_FIELDtype,dt:real) subroutine
 depose_current_em2d(np:integer,xp(np):real,yp(np):real,
                     uxp(np):real,uyp(np):real,uzp(np):real,gaminv(np):real,
@@ -180,6 +85,7 @@ geteb_em2d(np:integer,xp(np):real,yp(np):real,
 em2d_step() subroutine
 griuni(f:EM2D_FIELDtype) subroutine
 grimax(f:EM2D_FIELDtype) subroutine
+smooth2d_lindman(q(0:nx+2,0:ny+2),nx,ny) subroutine
 
 %%%%%%%% type_bnd:
 n integer
@@ -243,6 +149,8 @@ sinteta real
 cst1 real
 cst2 real
 cj(2) _real
+clight real
+mu0    real
 Ex(0:nx+3,0:ny+2) _real
 Ey(0:nx+3,0:ny+2) _real
 Ez(0:nx+3,0:ny+2) _real
@@ -259,6 +167,7 @@ Bz_in(0:ny+2) _real
 Ey_in(0:ny+2) _real
 Ex_in(0:ny+2) _real
 Ez_in(0:nx+3) _real
+Ez_in_part2(0:nx+3) _real
 By_in(0:nx+3) _real
 Bx_in(0:nx+3) _real
 profx(0:nx+3) _real
@@ -268,4 +177,3 @@ tpulse(0:npulse+1) _real
 pulse(0:npulse+1) _real
 bndexeybz _type_bnd
 bndbxbyez _type_bnd
-
