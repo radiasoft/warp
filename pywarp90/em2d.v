6125cab268029861b6d805b3f1022ac3dd4f7703
jlvay <jlvay>
2006-10-25 23:07:52 +0000

    Moved things around.

--- pywarp90/em2d.v
+++ pywarp90/em2d.v
@@ -1,5 +1,5 @@
 em2d
-#@(#) File EM2D.V, version $Revision: 1.11 $, $Date: 2006/09/07 00:40:15 $
+#@(#) File EM2D.V, version $Revision: 1.12 $, $Date: 2006/10/25 23:07:52 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -33,9 +33,6 @@ bndbxbyezc       _type_bnd
 bndbxbyezf       _type_bnd
 
 *********** EM2D_FIELDobjects dump:
-field        _EM2D_FIELDtype
-fpatchcoarse _EM2D_FIELDtype
-fpatchfine   _EM2D_FIELDtype
 l_onegrid                    logical /.true./
 l_elaser_out_plane           logical /.false./
 l_moving_window              logical /.false./
@@ -51,24 +48,8 @@ ixpatch                      integer /0/
 iypatch                      integer /0/
 ntamp_scatter                integer /2/
 ntamp_gather                 integer /4/
-xminpatch_scatter            real /0./
-xmaxpatch_scatter            real /0./
-yminpatch_scatter            real /0./
-ymaxpatch_scatter            real /0./
-xminpatch_gather             real /0./
-xmaxpatch_gather             real /0./
-yminpatch_gather             real /0./
-ymaxpatch_gather             real /0./
 transition_zone              real /0./ # length of zone for linear transition from coarse to fine force (in coarse cell units)
 tmin_moving_main_window      real /0./
-nxfsum integer /0/
-nyfsum integer /0/
-Exfsum(0:nxfsum+3,0:nyfsum+2) _real
-Eyfsum(0:nxfsum+3,0:nyfsum+2) _real
-Ezfsum(0:nxfsum+3,0:nyfsum+2) _real
-Bxfsum(0:nxfsum+3,0:nyfsum+2) _real
-Byfsum(0:nxfsum+3,0:nyfsum+2) _real
-Bzfsum(0:nxfsum+3,0:nyfsum+2) _real
 
 init_fields(f:EM2D_FIELDtype, nx:integer, ny:integer, 
            nbndx:integer, nbndy:integer, 
@@ -79,7 +60,8 @@ push_em_e(f:EM2D_FIELDtype,dt:real) subroutine
 push_em_b(f:EM2D_FIELDtype,dt:real) subroutine
 depose_current_em2d(n:integer,x(n):real,y(n):real,
                     ux(n):real,uy(n):real,uz(n):real,gaminv(n):real,
-                    w(n):real,q:real,dt:real,l_particles_weight:logical) subroutine
+                    w(n):real,q:real,dt:real,l_particles_weight:logical,
+                    field:EM2D_FIELDtype,fpatchfine:EM2D_FIELDtype) subroutine
 em2d_geteb2d_linear_serial(n:integer,x(n):real,y(n):real,
                            ex(n):real,ey(n):real,ez(n):real,
                            bx(n):real,by(n):real,bz(n):real,
@@ -89,18 +71,12 @@ em2d_geteb2d_linear_serial(n:integer,x(n):real,y(n):real,
                            ezg(0:nx+3,0:ny+2):real,
                            bxg(0:nx+3,0:ny+2):real,byg(0:nx+3,0:ny+2):real,
                            bzg(0:nx+3,0:ny+2):real) subroutine
-em2d_gete2d_linear_serial(n:integer,x(n):real,y(n):real,
-                          ex(n):real,ey(n):real,ez(n):real,
+em2d_getf2d_linear_serial(n:integer,x(n):real,y(n):real,
+                          fx(n):real,fy(n):real,fz(n):real,
                           xmin:real,ymin:real,dx:real,dy:real,
                           nx:integer,ny:integer,
-                          exg(0:nx+3,0:ny+2):real,eyg(0:nx+3,0:ny+2):real,
-                          ezg(0:nx+3,0:ny+2):real) subroutine
-em2d_getb2d_linear_serial(n:integer,x(n):real,y(n):real,
-                          bx(n):real,by(n):real,bz(n):real,
-                          xmin:real,ymin:real,dx:real,dy:real,
-                          nx:integer,ny:integer,
-                          bxg(0:nx+3,0:ny+2):real,byg(0:nx+3,0:ny+2):real,
-                          bzg(0:nx+3,0:ny+2):real) subroutine
+                          fxg(0:nx+3,0:ny+2):real,fyg(0:nx+3,0:ny+2):real,
+                          fzg(0:nx+3,0:ny+2):real) subroutine
 em2d_depose_jxjy_esirkepov_linear_serial(j:real,
                            n:integer,x(n):real,y(n):real,
                            ux(n):real,uy(n):real,uz(n):real,
@@ -108,15 +84,17 @@ em2d_depose_jxjy_esirkepov_linear_serial(j:real,
                            xmin:real,ymin:real,dt:real,dx:real,dy:real,
                            nx:integer,ny:integer,l_particles_weight:logical)
                            subroutine
-geteb_em2d(f:EM2D_FIELDtype,n:integer,x(n):real,y(n):real,
-           ex(n):real,ey(n):real,ez(n):real,
-           bx(n):real,by(n):real,bz(n):real) subroutine   
+getf_em2d(n:integer,x(n):real,y(n):real,
+           fx(n):real,fy(n):real,fz(n):real,
+           field:EM2D_FIELDtype,fpatchfine:EM2D_FIELDtype,
+           which:integer) subroutine   
 em2d_step() subroutine
 griuni(f:EM2D_FIELDtype) subroutine
 grimax(f:EM2D_FIELDtype) subroutine
 smooth2d_lindman(q(0:nx+3,0:ny+2),nx,ny) subroutine
-em2d_smoothdensity() subroutine
 move_window_field(f:EM2D_FIELDtype) subroutine
+project_j(f:EM2D_FIELDtype,fc:EM2D_FIELDtype,ff:EM2D_FIELDtype) subroutine
+set_substitute_fields(field:EM2D_FIELDtype,fpatchcoarse:EM2D_FIELDtype,fpatchfine:EM2D_FIELDtype) subroutine
 
 %%%%%%%% type_bnd:
 n integer
@@ -160,6 +138,14 @@ xmin real
 ymin real
 xmax real
 ymax real
+xminpatch_scatter            real /0./
+xmaxpatch_scatter            real /0./
+yminpatch_scatter            real /0./
+ymaxpatch_scatter            real /0./
+xminpatch_gather             real /0./
+xmaxpatch_gather             real /0./
+yminpatch_gather             real /0./
+ymaxpatch_gather             real /0./
 rap integer
 dx real
 dy real
@@ -194,17 +180,23 @@ Excopy(0:nxcopy+3,0:nycopy+2) _real
 Eycopy(0:nxcopy+3,0:nycopy+2) _real
 Bzcopy(0:nxcopy+3,0:nycopy+2) _real
 J(0:nx+3,0:ny+2,3) _real
+nxfsum integer /0/
+nyfsum integer /0/
+Exfsum(0:nxfsum+3,0:nyfsum+2) _real
+Eyfsum(0:nxfsum+3,0:nyfsum+2) _real
+Ezfsum(0:nxfsum+3,0:nyfsum+2) _real
+Bxfsum(0:nxfsum+3,0:nyfsum+2) _real
+Byfsum(0:nxfsum+3,0:nyfsum+2) _real
+Bzfsum(0:nxfsum+3,0:nyfsum+2) _real
 rm1(ny) _real
 rm2(ny) _real
 Bz_in(0:ny+2) _real
 Ey_in(0:ny+2) _real
 Ex_in(0:ny+2) _real
-Ez_in(0:nx+3) _real
-Ez_in_part2(0:nx+3) _real
-By_in(0:nx+3) _real
-Bx_in(0:nx+3) _real
-profx(0:nx+3) _real
-profy(0:ny+2) _real
+Ez_in(0:ny+2) _real
+By_in(0:ny+2) _real
+Bx_in(0:ny+2) _real
+laser_profile(0:ny+3) _real
 temp(0:ntemp) _real
 tpulse(0:npulse+1) _real
 pulse(0:npulse+1) _real
