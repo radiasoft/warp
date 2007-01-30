d899fc9483235ff9ee7e76237176c8e16306ef00
dave <dave>
2007-01-30 01:36:20 +0000

    Added a reference to setrho3dw.

--- pywarp90/w3d.v
+++ pywarp90/w3d.v
@@ -1,5 +1,5 @@
 w3d
-#@(#) File W3D.V, version $Revision: 3.249 $, $Date: 2006/12/19 22:38:05 $
+#@(#) File W3D.V, version $Revision: 3.250 $, $Date: 2007/01/30 01:36:20 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package W3D of code WARP
@@ -12,7 +12,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 
 *********** W3Dversion:
 # Quantities associated with version control 
-versw3d character*19 /"$Revision: 3.249 $"/ # Current code version, set by CVS
+versw3d character*19 /"$Revision: 3.250 $"/ # Current code version, set by CVS
 
 *********** Obsolete3d:
 inj_d                real /0/ # Obsolete, now see inj_d in top
@@ -867,6 +867,14 @@ setrho3d(rho(0:nx,0:ny,0:nz):real,rho1d:real,
          dx:real,dy:real,dz:real,xmmin:real,ymmin:real,zmmin:real,
          l2symtry:logical,l4symtry:logical,lcylindrical:logical)
              subroutine # Computes charge density
+setrho3dw(rho(0:nx,0:ny,0:nz):real,rho1d:real,
+          np,xp(np):real,yp(np):real,zp(np):real,zgrid:real,uzp(np):real,
+          wfact(np):real,
+          q:real,wght:real,depos:string,nx:integer,ny:integer,nz:integer,
+          dx:real,dy:real,dz:real,xmmin:real,ymmin:real,zmmin:real,
+          l2symtry:logical,l4symtry:logical,lcylindrical:logical)
+             subroutine # Computes charge density
+                        # using variable weighted particles
 setrho3dselect(rho(0:nx,0:ny,0:nz):real,rho1d:real,
                np,xp(np):real,yp(np):real,zp(np):real,zgrid:real,uzp(np):real,
                q:real,wght:real,depos:string,nx:integer,ny:integer,nz:integer,
