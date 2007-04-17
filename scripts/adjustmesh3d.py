8c4113d341ffb6b525e421516a059ae7c76f3484
dave <dave>
2007-04-17 23:28:14 +0000

    Removed the argument list in calls to setupFields3dParticles to match the fortran.

--- scripts/adjustmesh3d.py
+++ scripts/adjustmesh3d.py
@@ -5,7 +5,7 @@ adjustmeshz: Adjust the longitudinal length of the mesh.
 adjustmeshxy: Adjust the longitudinal length of the mesh.
 """
 from warp import *
-adjustmesh3d_version = "$Id: adjustmesh3d.py,v 1.24 2006/12/19 22:45:04 dave Exp $"
+adjustmesh3d_version = "$Id: adjustmesh3d.py,v 1.25 2007/04/17 23:28:14 dave Exp $"
 
 def adjustmesh3ddoc():
   import adjustmesh3d
@@ -59,7 +59,7 @@ Warning - this does not yet work in parallel
     gallot("SelfFieldGrid3d")
   except:
     gallot("Fields3d")
-  setupfields3dparticles(top.ns,top.ndts,top.it)
+  setupFields3dParticles()
   if w3d.solvergeom is w3d.RZgeom:
     frz.del_base()
     frz.init_base(w3d.nx,w3d.nz,w3d.dx,w3d.dz,w3d.xmmin,w3d.zmmin)
@@ -184,7 +184,7 @@ Resizes the transverse size of the mesh
 
   # --- Reallocate the fields
   gallot("Fields3d")
-  setupfields3dparticles(top.ns,top.ndts,top.it)
+  setupFields3dParticles()
   if w3d.solvergeom is w3d.RZgeom:
     frz.del_base()
     frz.init_base(w3d.nx,w3d.nz,w3d.dx,w3d.dz,w3d.xmmin,w3d.zmmin)
