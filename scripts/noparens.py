d3542eee427bcd999ced77ca834445ddfd9aefbe
dave <dave>
2007-06-21 16:47:36 +0000

    The object now grabs the documentation from the underlying function.

--- scripts/noparens.py
+++ scripts/noparens.py
@@ -1,12 +1,13 @@
 # Creates a class representation of functions that allow
 # the function to be called without the added '()' when there are no
 # arguments. Note that in that case, an extra blank line is output.
-noparens_version = "$Id: noparens.py,v 1.2 2001/01/11 00:21:48 dave Exp $"
+noparens_version = "$Id: noparens.py,v 1.3 2007/06/21 16:47:36 dave Exp $"
 
 
 class Noparens:
   def __init__(self,func):
     self.func = func
+    self.__doc__ = func.__doc__
   def __repr__(self):
     self.func()
     return ''
