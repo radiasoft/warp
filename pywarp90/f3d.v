5ebbfbcabbecddf7a7d037b2daee51698cbc6e8f
dave <dave>
2007-06-26 01:14:53 +0000

    Moved the code setting up the icgrid for the field at subgridpoints into the new routine setupconductorfielddata.

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.174 $, $Date: 2007/06/22 21:50:06 $
+#@(#) File F3D.V, version $Revision: 3.175 $, $Date: 2007/06/26 01:14:53 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.174 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.175 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -329,6 +329,8 @@ subcond_sumrhointerior(rhosum:real,interior:ConductorInteriorType,
                     ixmin:integer,ixmax:integer,iymin:integer,iymax:integer,
                     izmin:integer,izmax:integer) subroutine
 
+setupconductorfielddata(conductors:ConductorType,nx:integer,ny:integer,nzlocal:integer)
+                    subroutine
 getefieldatconductors(conductors:ConductorType,phi:real,
                       dx:real,dy:real,dz:real,nx:integer,ny:integer,nz:integer,
                       delx:integer,dely:integer,delz:integer)
