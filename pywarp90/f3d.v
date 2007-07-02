63c8ffe49ac60664c24f095f622832d0e1f7ea3c
dave <dave>
2007-07-02 10:58:07 +0000

    Added timecheckconductors

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.176 $, $Date: 2007/06/26 01:36:34 $
+#@(#) File F3D.V, version $Revision: 3.177 $, $Date: 2007/07/02 10:58:07 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.176 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.177 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -967,6 +967,7 @@ timegathersourcefromchild real /0./
 timegatherpotentialfromparents real /0./
 timegatherafromparents real /0./
 timegatherjfromchild real /0./
+timecheckconductors real /0./
 
 timeexchange_phi           real /0./
 timetranspose              real /0./
