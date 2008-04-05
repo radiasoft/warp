24876147c5012c887ea559e9f8e9524c1f64c280
dave <dave>
2008-04-05 00:42:10 +0000

    Obsolete - part of old pyBasis code

--- scripts/ncgpy.py
+++
@@ -1,8 +0,0 @@
-import Numeric
-ncgpy_version = "$Id: ncgpy.py,v 1.1 2000/10/16 18:34:19 dave Exp $"
-
-class Ncg:
-  def __init__(self):
-    self.versncg = Numeric.array(["2",".","0"])
-
-ncg = Ncg()
