1be01422ee2ceb53907e4381a041b44066dc2c66
dave <dave>
2008-10-22 01:03:44 +0000

    Updated arguments to expand3d

--- pywarp90/f3d.v
+++ pywarp90/f3d.v
@@ -1,5 +1,5 @@
 f3d
-#@(#) File F3D.V, version $Revision: 3.193 $, $Date: 2008/10/15 17:59:14 $
+#@(#) File F3D.V, version $Revision: 3.194 $, $Date: 2008/10/22 01:04:50 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package F3D of code WARP6
@@ -10,7 +10,7 @@ LARGEPOS = 1.0e+36 # This must be the same as in top.v
 }
 
 *********** F3Dversion:
-versf3d character*19 /"$Revision: 3.193 $"/#  Code version version is set by CVS
+versf3d character*19 /"$Revision: 3.194 $"/#  Code version version is set by CVS
 
 *********** F3Dvars:
 # Variables needed by the test driver of package F3D
@@ -351,7 +351,8 @@ expand3d(nx:integer,ny:integer,nzlocal:integer,nz:integer,
          nxcoarse:integer,nycoarse:integer,nzlocalcoarse:integer,
          nzcoarse:integer,
          phicoarse(0:nxcoarse,0:nycoarse,-1:nzlocalcoarse+1):real,
-         bounds(0:5):integer,lzoffset:integer)
+         bounds(0:5):integer,lzoffset:integer,
+         conductors:ConductorType,lprecalccoeffs:logical)
    subroutine
    # Expands phi in 3 dimensiosn
 sorhalfpass3d(parity:integer,mglevel:integer,
