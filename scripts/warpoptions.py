f1ab2392ed67c3753c62898e72a47d8483c93959
dave <dave>
2008-12-16 21:54:40 +0000

    Added the localflags option

--- scripts/warpoptions.py
+++ scripts/warpoptions.py
@@ -1,7 +1,7 @@
 """Command line options for Warp
 """
 import optparse
-warpoptions_version = "$Id: warpoptions.py,v 1.1 2008/11/19 22:22:45 dave Exp $"
+warpoptions_version = "$Id: warpoptions.py,v 1.2 2008/12/16 21:54:40 dave Exp $"
 
 def warpoptionsdoc():
   import warpoptions
@@ -13,5 +13,7 @@ parser = optparse.OptionParser()
 
 parser.add_option('-p','--decomp',type='int',nargs=3,dest='decomp',
                   help='Set the 3-D decomposition, nxprocs nyprocs nzprocs')
+parser.add_option('-l','--localflags',dest='localflags',
+                  help='localflags file to execfile')
 
 options, args = parser.parse_args()
