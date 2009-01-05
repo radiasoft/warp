2901a07663f0813f3348231a369376a51e9151ac
dave <dave>
2009-01-05 19:19:51 +0000

    Added some prototyping information

--- pywarp90/ranffortran.m
+++ pywarp90/ranffortran.m
@@ -1,14 +1,14 @@
 /* Created by David P. Grote, January 1, 2004 */
-/* $Id: ranffortran.m,v 1.2 2009/01/05 19:10:17 dave Exp $ */
+/* $Id: ranffortran.m,v 1.3 2009/01/05 19:19:51 dave Exp $ */
 
 /* This is needed since the ranf in ranlib is single precision */
-extern double Ranf();
-%'double '+fname(py_ifelse(f90 or f90f,1,'w','')+'ranf')+'()'
+extern double Ranf(void);
+%'double '+fname(py_ifelse(f90 or f90f,1,'w','')+'ranf')+'(void)'
 {
   return Ranf();
 }
 
-%py_ifelse(machine,'T3E','extern void RANSET();','extern void Seedranf();')
+%py_ifelse(machine,'T3E','extern void RANSET(double *x);','extern void Seedranf(double *x);')
 %'void '+fname('seedranf')+'(double* x)'
 {
 %py_ifelse(machine,'T3E','RANSET(x);','Seedranf(x);')
