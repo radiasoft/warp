8e5cedb2ee009ff8382feb90ae88ca50867e64b6
dave <dave>
2009-01-26 22:32:47 +0000

    Added the lrz argument to addpart.

--- pywarp90/top.v
+++ pywarp90/top.v
@@ -1,5 +1,5 @@
 top
-#@(#) File TOP.V, version $Revision: 3.252 $, $Date: 2009/01/24 01:50:33 $
+#@(#) File TOP.V, version $Revision: 3.253 $, $Date: 2009/01/26 22:32:47 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -60,7 +60,7 @@ codeid   character*8  /"warp r2"/     # Name of code, and major version
 
 *********** TOPversion:
 # Version control for global commons
-verstop character*19 /"$Revision: 3.252 $"/ # Global common version, set by CVS
+verstop character*19 /"$Revision: 3.253 $"/ # Global common version, set by CVS
 
 *********** Machine_param:
 wordsize integer /64/ # Wordsize on current machine--used in bas.wrp
@@ -2711,7 +2711,8 @@ addpart(pgroup:ParticleGroup,nn:integer,npid:integer,
         xmmin:real,xmmax:real,
         ymmin:real,ymmax:real,
         zmmin:real,zmmax:real,
-        lmomentum:logical,lfields:logical,l2symtry:logical,l4symtry:logical)
+        lmomentum:logical,lfields:logical,
+        l2symtry:logical,l4symtry:logical,lrz:logical)
              subroutine # Adds new particles to the simulation
 clearpart(pgroup:ParticleGroup,js:integer,fillmethod:integer)
              subroutine # Clears away lost particles.
