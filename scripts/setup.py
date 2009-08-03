1446c8e7461f663c7815ff69702b0068bbb14244
dave <dave>
2009-08-03 17:44:22 +0000

    setup for distutils and creating an egg file.

---
+++ scripts/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# To use:
+#       python setup.py install
+#
+
+try:
+    # --- For installing into site-packages, with python setup.py install
+    #from distutils.core import setup
+    # --- For creating an egg file, with python setup.py bdist_egg
+    from setuptools import setup
+except:
+    raise SystemExit, 'Distutils problem'
+
+setup (name = 'warp',
+       author = 'David P. Grote, Jean-Luc Vay, et. al.',
+       author_email = 'DPGrote@lbl.gov',
+       description = 'Warp scripts',
+       long_description = """
+Warp scripts""",
+       platforms = 'Linux, Unix, Windows (cygwin), Mac OSX',
+       packages = ['warp'], #,'warp.GUI'],
+       package_dir = {'warp': '.'}
+       )
