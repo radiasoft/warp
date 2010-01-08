f4b25673b1c9f11116394062b01b0276305a758a
dave <dave>
2010-01-08 18:11:31 +0000

    Added the --pnumb option

--- scripts/warpoptions.py
+++ scripts/warpoptions.py
@@ -3,7 +3,7 @@ Users can add their own options by importing warpoptions (before importing
 warp) add calling parser.add_option.
 """
 import optparse
-warpoptions_version = "$Id: warpoptions.py,v 1.3 2009/06/12 19:06:21 dave Exp $"
+warpoptions_version = "$Id: warpoptions.py,v 1.4 2010/01/08 18:11:31 dave Exp $"
 
 def warpoptionsdoc():
     import warpoptions
@@ -17,6 +17,9 @@ parser.add_option('-p','--decomp',type='int',nargs=3,dest='decomp',
                   help='Set the 3-D decomposition, nxprocs nyprocs nzprocs')
 parser.add_option('-l','--localflags',dest='localflags',
                   help='localflags file to execfile')
+parser.add_option('--pnumb',dest='pnumb',type='string',default=None,
+                  help='Run number, used in the plot and other output file names.')
+
 
 def parse_args():
     global options, args
