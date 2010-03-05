75435c5a2cbc6b41d3350b2c98392309a640d36f
dave <dave>
2010-03-05 01:02:56 +0000

    Various changes and additions:  - added maxvoltagechangerate and sourcevoltinit  - setsourcevolt is now called beforefs. This is nicer since it leaves    the conductor voltages at there correct value and is a little cleaner.  - in SpecifiedCurrentRiseTime, removed the installation of setphiref    replacing it with a version of setsourcevolt that calls setphiref.  - minor code clean up  - added some additional commentation

--- scripts/constantcurrentinjection.py
+++ scripts/constantcurrentinjection.py
@@ -4,7 +4,7 @@ procedure, but using the full simulation instead of 1-D approximation.
 from warp import *
 from timedependentvoltage import TimeVoltage
 
-constantcurrentinjection_version = "$Id: constantcurrentinjection.py,v 1.8 2009/11/18 22:09:32 jlvay Exp $"
+constantcurrentinjection_version = "$Id: constantcurrentinjection.py,v 1.9 2010/03/05 01:02:56 dave Exp $"
 def constantcurrentinjectiondoc():
   import constantcurrentinjection
   print constantcurrentinjection.__doc__
@@ -38,6 +38,17 @@ Input arguements:
  - endplatevolt=0.: voltage on plate at end of diode. In a triode
                     configuration, or a diode with a guard plate, this should
                     be the voltage on the guard plate.
+ - maxvoltagechangerate=None: optional maximum rate at which the source voltage
+                              can be varied. If this is not specified, the
+                              change in the voltage in unconstrained. This
+                              will mostly affect the initial voltage. Without
+                              the constraint, the initial voltage will just be
+                              whatever it comes out to be. With the
+                              constraint, the initial voltage will be relative
+                              to the given value, sourcevoltinit.
+ - sourcevoltinit=endplatevolt: Initial value of the source voltage. This is
+                                only used when maxvoltagechangerate is
+                                specified.
 
 This does the equivalent of setting the following parameters
 frz.l_find_rise_time = true
@@ -46,9 +57,11 @@ frz.v_max = ekininit
 frz.calc_a = 3
   """
 
-  def __init__(self,sourceid,currentdensity,sourcevolt,otherids=[],othervolts=[],
-                    othercontrolledids=[],othercontrolledvolts=[],endplatevolt=0.,
-                    l_setvinject=1):
+  def __init__(self,sourceid,currentdensity,sourcevolt,
+               otherids=[],othervolts=[],
+               othercontrolledids=[],othercontrolledvolts=[],endplatevolt=0.,
+               l_setvinject=1,
+               maxvoltagechangerate=None,sourcevoltinit=None):
     self.currentdensity = currentdensity
     self.sourceid = sourceid
     self.sourcevolt = sourcevolt
@@ -58,9 +71,11 @@ frz.calc_a = 3
     self.othercontrolledvolts = othercontrolledvolts
     self.endplatevolt = endplatevolt
     self.l_setvinject = l_setvinject
-    self.hphiref = []
+    self.maxvoltagechangerate = maxvoltagechangerate
+    self.sourcevoltinit = sourcevoltinit or sourcevolt
 
     # --- Initialize parameters
+    self.hphiref = []
     self.setphiref(currentdensity)
     print 'phiref = ',self.phiref
     if w3d.l_inj_rz:
@@ -70,10 +85,14 @@ frz.calc_a = 3
       self.ww = w3d.inj_dx*w3d.inj_dy*w3d.inj_area[:,:,0]
     self.wwsum = sum(self.ww)
 
-    # --- First set all but source to ground
+    # --- Calculate the potentials with only the source and controlled
+    # --- conductors charged to their steady-state value and everything
+    # --- else at endplatevolt. Though everything is shifted down by
+    # --- endplatevolt for convenience, since it is assumed that there
+    # --- will be more conductors around, but all at ground.
     setconductorvoltage(sourcevolt-self.endplatevolt,condid=self.sourceid)
-    for i,id in enumerate(othercontrolledids):
-      setconductorvoltage(self.othercontrolledvolts[i],condid=id)
+    for id,v in zip(othercontrolledids,othercontrolledvolts):
+      setconductorvoltage(v-self.endplatevolt,condid=id)
     for id in otherids:
       setconductorvoltage(0.,condid=id)
 
@@ -100,12 +119,9 @@ frz.calc_a = 3
     else:
       self.phiv = sum(self.ww*w3d.inj_phi[:,:,0])/self.wwsum
 
-    # --- Now reset voltages on plates and set source to voltage of endplatevolt
-    setconductorvoltage(self.endplatevolt,condid=self.sourceid)
-    for id,v in map(None,otherids,othervolts):
+    # --- Restore the voltage on the other conductors.
+    for id,v in zip(otherids,othervolts):
       setconductorvoltage(v,condid=id)
-    for id in othercontrolledids:
-      setconductorvoltage(self.endplatevolt,condid=id)
 
     # --- Setup histories
     self.hsourcevolt = AppendableArray(typecode='d')
@@ -113,26 +129,42 @@ frz.calc_a = 3
     self.hphirho = AppendableArray(typecode='d')
     self.hnp = AppendableArray(typecode='d')
     self.htime = AppendableArray(typecode='d')
+    self.hafactunconstrained = AppendableArray(typecode='d')
 
     # --- Make initial call
-    fieldsol(-1)
     self.setsourcevolt()
 
-    # --- Install setsourcevolt so it is called after a fieldsolve
-    installafterfs(self.setsourcevolt)
+    # --- Install setsourcevolt so it is called before a fieldsolve
+    # --- Calling setsourcevolt beforefs leaves the voltages on the
+    # --- conductors at their correct values.
+    installbeforefs(self.setsourcevolt)
 
   def setsourcevolt(self):
-    if top.inject==0:return
+    if top.inject==0: return
+
+    # --- Set the conductor voltages at the endpatevolt so that
+    # --- the field solve will not include the diode voltage.
+    # --- It will include the potential from the space charge, plus
+    # --- any voltage that leaks through the diode end plate.
+    setconductorvoltage(self.endplatevolt,condid=self.sourceid)
+    for id in self.othercontrolledids:
+      setconductorvoltage(self.endplatevolt,condid=id)
+
     # --- Calculate phirho
-    #fieldsol(-1) # done afterfs
-    if self.l_setvinject:top.vinject = self.endplatevolt
+    fieldsol(-1)
+
+    if self.l_setvinject:
+      top.vinject = self.endplatevolt
     getinj_phi()
+
     if w3d.l_inj_rz:
       phirho = -sum(self.ww*w3d.inj_phi[:,0,0])/self.wwsum
     else:
       phirho = -sum(self.ww*w3d.inj_phi[:,:,0])/self.wwsum
 
     self.afact = (self.phiref + phirho)/self.phiv
+    self.hafactunconstrained.append(self.afact)
+
     solver = getregisteredsolver()
     if solver is None:
       solver = frz.basegrid
@@ -141,38 +173,33 @@ frz.calc_a = 3
       for c in self.blocklists[i]:
         if c.isactive:
           c.phi += self.afact*c.phisave
+
     voltage = (self.endplatevolt +
                    self.afact*(self.sourcevolt-self.endplatevolt))
+
+    # --- The voltage may be constrained by maxvoltagechangerate.
+    # --- Note that this modifies self.afact.
+    voltage = self.constrainvoltage(voltage)
+
     if self.l_setvinject:
       top.vinject = voltage
     setconductorvoltage(voltage,condid=self.sourceid)
-    for id,v in map(None,self.othercontrolledids,self.othercontrolledvolts):
+    for id,v in zip(self.othercontrolledids,self.othercontrolledvolts):
       voltage = (self.endplatevolt +
                      self.afact*(v-self.endplatevolt))
       setconductorvoltage(voltage,condid=id)
-    fieldsol(-1)
-    getinj_phi()
-
-    # --- Leave the conductor voltages at the endpatevolt so that
-    # --- the automatic field solve will not include the diode voltage which
-    # --- is added in in this routine.
-    setconductorvoltage(self.endplatevolt,condid=self.sourceid)
-    for id in self.othercontrolledids:
-      setconductorvoltage(self.endplatevolt,condid=id)
 
     self.hsourcevolt.append(voltage)
     self.hafact.append(self.afact)
     self.hphirho.append(phirho)
     self.hnp.append(getn())
     self.htime.append(top.time)
-    import os
-
 
   def disable(self):
-    if isinstalledafterfs(self.setsourcevolt):
-      uninstallafterfs(self.setsourcevolt)
+    if isinstalledbeforefs(self.setsourcevolt):
+      uninstallbeforefs(self.setsourcevolt)
       setconductorvoltage(self.sourcevolt,condid=self.sourceid)
-      for id,v in map(None,self.othercontrolledids,self.othercontrolledvolts):
+      for id,v in zip(self.othercontrolledids,self.othercontrolledvolts):
         setconductorvoltage(v,condid=id)
 
   def plothist(self,tscale=1.,vscale=1.,tunits='s',vunits='V',title=1):
@@ -181,10 +208,7 @@ frz.calc_a = 3
     
   def setphiref(self,currentdensity=None):
     if top.inject==0:return
-    if currentdensity is None:
-      self.currentdensity = self.currentdensityfunc(top.time)
-    else:
-      self.currentdensity = currentdensity
+    self.currentdensity = currentdensity
 
     chi = 4*eps0/9*sqrt(2*top.pgroup.sq[0]/top.pgroup.sm[0])
     d = top.inj_d[0]*w3d.inj_dz
@@ -211,19 +235,51 @@ frz.calc_a = 3
 
     self.hphiref.append(self.phiref)
     
+  def constrainvoltage(self,voltage):
+    # --- Do nothing if there is no constraint.
+    if self.maxvoltagechangerate is None: return voltage
+
+    if len(self.hsourcevolt) == 0:
+      # --- Use the initial voltage if this is the first call.
+      voltage = self.sourcevoltinit
+    else:
+      # --- Constrain the voltage to be within the rate of change of the
+      # --- previous voltage.
+      vprevious = self.hsourcevolt[-1]
+      dvmax = self.maxvoltagechangerate*top.dt
+      voltage = min(voltage,vprevious+dvmax)
+      voltage = max(voltage,vprevious-dvmax)
+      #if len(self.hsourcevolt) > 1:
+      #  dv = 2*self.hsourcevolt[-1] - self.hsourcevolt[-2]
+      #  voltage = max(-2.+dv,min(+2.+dv,voltage))
+
+    # --- Adjust afact so that it would have calculated the constrained
+    # --- voltage. This is only needed if there are other controlled voltages.
+    self.afact = ((voltage - self.endplatevolt)/
+                  (self.sourcevolt-self.endplatevolt))
+
+    return voltage
+
 class SpecifiedCurrentRiseTime(ConstantCurrentRiseTime):
   """
 Same as constantcurrentinjection but allows to specify time dependent current profile.
   """
-  def __init__(self,sourceid,currentdensityfunc,sourcevolt,otherids=[],othervolts=[],
-                    othercontrolledids=[],othercontrolledvolts=[],endplatevolt=0.,
-                    l_setvinject=1):
-    ConstantCurrentRiseTime.__init__(self,sourceid,currentdensityfunc(top.time),sourcevolt,
-                                          otherids,othervolts,
-                                          othercontrolledids,othercontrolledvolts,endplatevolt,
-                                          l_setvinject)
+  def __init__(self,sourceid,currentdensityfunc,sourcevolt,
+               otherids=[],othervolts=[],
+               othercontrolledids=[],othercontrolledvolts=[],endplatevolt=0.,
+               l_setvinject=1,
+               maxvoltagechangerate=None,sourcevoltinit=None):
     self.currentdensityfunc = currentdensityfunc
-    installbeforefs(self.setphiref)
-    self.hphiref = AppendableArray(typecode='d')
-    
+    ConstantCurrentRiseTime.__init__(self,sourceid,
+                                     currentdensityfunc(top.time),sourcevolt,
+                                     otherids,othervolts,
+                                     othercontrolledids,othercontrolledvolts,
+                                     endplatevolt,
+                                     l_setvinject,
+                                     maxvoltagechangerate,
+                                     sourcevoltinit)
+
+  def setsourcevolt(self):
+    self.setphiref(self.currentdensityfunc(top.time))
+    ConstantCurrentRiseTime.setsourcevolt(self)
 
