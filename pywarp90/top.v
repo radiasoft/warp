72f3c0e909ea08b1f97a1d5b8ee1afae5584a7a1
jlvay <jlvay>
2010-03-26 14:24:43 +0000

    Added variable zbirthlabpid.

--- pywarp90/top.v
+++ pywarp90/top.v
@@ -1,5 +1,5 @@
 top
-#@(#) File TOP.V, version $Revision: 3.274 $, $Date: 2010/03/11 01:09:48 $
+#@(#) File TOP.V, version $Revision: 3.275 $, $Date: 2010/03/26 14:24:43 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -60,7 +60,7 @@ codeid   character*8  /"warp r2"/     # Name of code, and major version
 
 *********** TOPversion:
 # Version control for global commons
-verstop character*19 /"$Revision: 3.274 $"/ # Global common version, set by CVS
+verstop character*19 /"$Revision: 3.275 $"/ # Global common version, set by CVS
 
 *********** Machine_param:
 wordsize integer /64/ # Wordsize on current machine--used in bas.wrp
@@ -2179,6 +2179,9 @@ xbirthpid integer /0/  # position of particle birth x in array pid
 ybirthpid integer /0/  # position of particle birth y in array pid
                        # (FORTRAN indexed: based 1)
 zbirthpid integer /0/  # position of particle birth z in array pid
+                       # (FORTRAN indexed: based 1)
+zbirthlabpid integer /0/  # position of particle birth z in array pid
+                       # in laboratory frame
                        # (FORTRAN indexed: based 1)
 uxbirthpid integer /0/ # position of particle birth ux in array pid
                        # (FORTRAN indexed: based 1)
