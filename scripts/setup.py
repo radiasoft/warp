5dcdf8a55a118c7709951ef61fd703386aebbafd
dave <dave>
2010-10-12 23:31:54 +0000

    Added a hack which puts the two files warptions.py and parallel.py into separate packages. This is needed so that the two can be imported independently of warp.

--- scripts/setup.py
+++ scripts/setup.py
@@ -3,15 +3,31 @@
 #       python setup.py install
 #
 
-setup_version = "$Id: setup.py,v 1.3 2010/03/11 19:10:20 dave Exp $"
+import os
+
+setup_version = "$Id: setup.py,v 1.4 2010/10/12 23:31:54 dave Exp $"
 
 try:
     # --- For installing into site-packages, with python setup.py install
-    #from distutils.core import setup
+    from distutils.core import setup
     # --- For creating an egg file, with python setup.py bdist_egg
-    from setuptools import setup
+    #from setuptools import setup
 except:
     raise SystemExit, 'Distutils problem'
+
+# --- Hack warning:
+# --- Put warpoptions.py and parallel.py into subdirectories so that they
+# --- can be separate packages. This is needed so that the two modules can be
+# --- imported independently of (and before) warp.
+os.renames('warpoptions.py','warpoptions/warpoptions.py')
+ff = open('warpoptions/__init__.py','w')
+ff.write('from warpoptions import *\n')
+ff.close()
+
+os.renames('parallel.py','parallel/parallel.py')
+ff = open('parallel/__init__.py','w')
+ff.write('from parallel import *\n')
+ff.close()
 
 setup (name = 'warp',
        author = 'David P. Grote, Jean-Luc Vay, et. al.',
@@ -20,7 +36,15 @@ setup (name = 'warp',
        long_description = """
 Warp scripts""",
        platforms = 'Linux, Unix, Windows (cygwin), Mac OSX',
-       packages = ['warp','warp.GUI'], # Note that the GUI info is incomplete
+       packages = ['warp','warp.GUI','warpoptions','parallel'],
        package_dir = {'warp': '.'},
        package_data = {'warp':['*.gs','*.gp']}
        )
+
+# --- Undo the hack from above
+os.remove('warpoptions/__init__.py')
+os.renames('warpoptions/warpoptions.py','warpoptions.py')
+
+os.remove('parallel/__init__.py')
+os.renames('parallel/parallel.py','parallel.py')
+
