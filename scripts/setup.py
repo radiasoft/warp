bf5070809f8b97b755970f31aecf59680a5a8f93
grote <grote>
2011-04-26 23:54:11 +0000

    Moved the code which undoes the moving of warpoptions and parallel into a finally clause so that it is always done, even if there are errors in setup.

--- scripts/setup.py
+++ scripts/setup.py
@@ -5,7 +5,7 @@
 
 import os
 
-setup_version = "$Id: setup.py,v 1.5 2010/10/13 19:17:21 dave Exp $"
+setup_version = "$Id: setup.py,v 1.6 2011/04/26 23:54:11 grote Exp $"
 
 try:
     # --- For installing into site-packages, with python setup.py install
@@ -22,19 +22,22 @@ except:
 os.renames('warpoptions.py','warpoptions/__init__.py')
 os.renames('parallel.py','parallel/__init__.py')
 
-setup (name = 'warp',
-       author = 'David P. Grote, Jean-Luc Vay, et. al.',
-       author_email = 'DPGrote@lbl.gov',
-       description = 'Warp scripts',
-       long_description = """
-Warp scripts""",
-       platforms = 'Linux, Unix, Windows (cygwin), Mac OSX',
-       packages = ['warp','warp.GUI','warpoptions','parallel'],
-       package_dir = {'warp': '.'},
-       package_data = {'warp':['*.gs','*.gp']}
-       )
+try:
+    setup (name = 'warp',
+           author = 'David P. Grote, Jean-Luc Vay, et. al.',
+           author_email = 'DPGrote@lbl.gov',
+           description = 'Warp scripts',
+           long_description = """
+    Warp scripts""",
+           platforms = 'Linux, Unix, Windows (cygwin), Mac OSX',
+           packages = ['warp','warp.GUI','warpoptions','parallel'],
+           package_dir = {'warp': '.'},
+           package_data = {'warp':['*.gs','*.gp']}
+           )
 
-# --- Undo the hack from above
-os.renames('warpoptions/__init__.py','warpoptions.py')
-os.renames('parallel/__init__.py','parallel.py')
+finally:
+    # --- Undo the hack from above. Inside the finally clause, this will
+    # --- always happen, even if there is an error during setup.
+    os.renames('warpoptions/__init__.py','warpoptions.py')
+    os.renames('parallel/__init__.py','parallel.py')
 
