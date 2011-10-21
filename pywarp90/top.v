06ab6b03996188e6794a1e91ebc620bc5dbf7181
grote <grote>
2011-10-21 18:00:39 +0000

    For gridcrossing moments, added the xmax, ymax and rmax diagnostics.

--- pywarp90/top.v
+++ pywarp90/top.v
@@ -1,5 +1,5 @@
 top
-#@(#) File TOP.V, version $Revision: 3.304 $, $Date: 2011/09/29 00:17:25 $
+#@(#) File TOP.V, version $Revision: 3.305 $, $Date: 2011/10/21 18:00:39 $
 # Copyright (c) 1990-1998, The Regents of the University of California.
 # All rights reserved.  See LEGAL.LLNL for full text and disclaimer.
 # This is the parameter and variable database for package TOP of code WARP
@@ -60,7 +60,7 @@ codeid   character*8  /"warp r2"/     # Name of code, and major version
 
 *********** TOPversion:
 # Version control for global commons
-verstop character*19 /"$Revision: 3.304 $"/ # Global common version, set by CVS
+verstop character*19 /"$Revision: 3.305 $"/ # Global common version, set by CVS
 
 *********** Machine_param:
 wordsize integer /64/ # Wordsize on current machine--used in bas.wrp
@@ -2308,6 +2308,9 @@ vysqbargc(0:ntgc,0:nzgc,0:nszgc-1) _real   # Vy**2 bar at grid crossing moments
 vzsqbargc(0:ntgc,0:nzgc,0:nszgc-1) _real   # Vz**2 bar at grid crossing moments
 xvxbargc(0:ntgc,0:nzgc,0:nszgc-1) _real   # XVx bar at grid crossing moments
 yvybargc(0:ntgc,0:nzgc,0:nszgc-1) _real   # YVy bar at grid crossing moments
+xmaxgc(0:ntgc,0:nzgc,0:nszgc-1) _real    # X max at grid crossing moments
+ymaxgc(0:ntgc,0:nzgc,0:nszgc-1) _real    # Y max at grid crossing moments
+rmaxgc(0:ntgc,0:nzgc,0:nszgc-1) _real    # R max at grid crossing moments
 
 *********** GridCrossing_Moments dump:
 # This allows up to two independent grid crossing moments diagnostics
